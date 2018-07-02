146cd63f2fd9f5bd840db9e201f8a46f5efb1cd4
Khalil Estel <khalil.estell@sjsu.edu>
2018-07-01 19:40:06 -0700

    Adding printf, scanf and their IO varients to repo
    _write, _read and the rest of the newlib required implementations have
    been added to the newlib file.
    
    Due to the size of the native printf function, the implementation has
    been reimplemented using vsprintf.
    
    scanf should be replaced with a vsscanf to reduce implementation size.

--- firmware/library/newlib/newlib.cpp
+++ firmware/library/newlib/newlib.cpp
@@ -1,6 +1,22 @@
+#include <cstdio>
 #include <cstdint>
-#include <cstdlib>
+#include <sys/stat.h>
+#include <unistd.h>
+#include "L0_LowLevel/uart0.min.hpp"
 
+// Dummy implementation of isatty
+extern "C" int _isatty(int)
+{
+    return 1;
+}
+// Dummy implementation of fstat, makes the assumption that the "device" 
+// representing, in this case STDIN, STDOUT, and STDERR as character devices.
+extern "C" int _fstat(int file, struct stat * status)
+{
+    (void)file;
+    status->st_mode = S_IFCHR;
+    return 0;
+}
 // Implementation of sbrk for allocating and deallocating space for malloc.
 extern "C" void * _sbrk(int increment)
 {
@@ -17,4 +33,45 @@ extern "C" void * _sbrk(int increment)
     heap_end += increment;
     previous_heap_end = reinterpret_cast<void *>(previous_heap_end);
     return previous_heap_end;
+}
+// Dummy implementation of close
+extern "C" int _close(int)
+{
+    return -1;
+}
+// Minimum implementation of _write using UART0 putchar
+extern "C" int _write(int file, char * ptr, int length)
+{
+    (void)file;
+    for (int i = 0; i < length; i++)
+    {
+        // TODO: either make this inline, or swap with function that can
+        //   take a buffer and length.
+        uart0_putchar(ptr[i]);
+    }
+    return length;
+}
+// Dummy implementation of _lseek
+extern "C" int _lseek(int file, int ptr, int dir)
+{
+    (void)file;
+    (void)ptr;
+    (void)dir;
+    return 0;
+}
+// Minimum implementation of _read using UART0 getchar
+extern "C" int _read(FILE * file, char * ptr, int length)
+{
+    if(file == STDIN_FILENO)
+    {
+        length = 1;
+        *ptr = uart0_getchar(0);
+        if(*ptr == '\r')
+        {
+            uart0_putchar('\r');
+            *ptr = '\n';
+        }
+        uart0_putchar(*ptr);
+    }
+    return length;
 }
