5ff1eaf21c2003d6ffb0855ea4a2287646be2889
Khalil Estel <khalil.estell@sjsu.edu>
2018-07-06 17:45:03 -0700

    Conforming uart0.min.hpp
    Refactored uart0.min.hpp to adhere to style guide.

--- firmware/library/L0_LowLevel/uart0.min.hpp
+++ firmware/library/L0_LowLevel/uart0.min.hpp
@@ -1,12 +1,9 @@
-#ifndef UART0_MIN_H
-#define UART0_MIN_H
+// Minimum required implementations of uart0 to make print statements work
+#pragma once
 
-#include <stdint.h>
 #include "LPC40xx.h"
 
 void uart0_init(uint32_t baud_rate);
 char uart0_getchar(char notused);
 char uart0_putchar(char out);
 void uart0_puts(const char c_string[]);
-
-#endif
