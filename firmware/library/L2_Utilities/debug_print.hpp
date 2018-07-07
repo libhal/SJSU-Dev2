8f5521ffac0b2b25e551a390dd1197f8b4b1aabf
Khalil Estel <khalil.estell@sjsu.edu>
2018-07-06 17:39:23 -0700

    Adding color to SJSU-Dev2
    HelloWorld project will now use colored output if you set SJ2_ENABLE_ANSI_CODES
    to true.
    
    DEBUG_PRINT has been enhanced to also use colors for the output.

--- firmware/library/L2_Utilities/debug_print.hpp
+++ firmware/library/L2_Utilities/debug_print.hpp
@@ -1,6 +1,9 @@
+// All
+#pragma once
 #include <cstdio>
-
+#include "L2_Utilities/ansi_terminal_codes.hpp"
 // Printf style logging with filename, function name, and line number
-#define DEBUG_PRINT(format, ...)                                        \
-    printf("%s:%s:%d> " format, __FILE__, __PRETTY_FUNCTION__, __LINE__, \
-           ##__VA_ARGS__);
+#define DEBUG_PRINT(format, ...)                                \
+    printf(SJ2_HI_BLUE "%s:" SJ2_HI_GREEN "%s:" SJ2_HI_YELLOW   \
+                       "%d> " SJ2_WHITE format SJ2_COLOR_RESET, \
+           __FILE__, __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
