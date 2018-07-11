638c4ef24499f7cae04437597b5ffd1bc1e60923
Khalil Estel <khalil.estell@sjsu.edu>
2018-07-10 18:22:58 -0700

    PinConfigure support for all IOCON register types
    The driver now supports type D, A, U, I, and W IOCON registers.

--- firmware/library/L1_Drivers/pin_configure.hpp
+++ firmware/library/L1_Drivers/pin_configure.hpp
@@ -22,12 +22,18 @@ class PinConfigureInterface
         kPullUp,
         kRepeater
     };
-    virtual void SetPinFunction(uint8_t function)         = 0;
-    virtual void SetPinMode(PinMode mode)                 = 0;
-    virtual void EnableHysteresis(bool enable_hysteresis) = 0;
-    virtual void SetAsActiveLow(bool set_as_active_low)   = 0;
-    virtual void EnableFastMode(bool enable_fast_mode)    = 0;
-    virtual void SetAsOpenDrain(bool set_as_open_drain)   = 0;
+    virtual void SetPinFunction(uint8_t function)                        = 0;
+    virtual void SetPinMode(PinConfigureInterface::PinMode mode)         = 0;
+    virtual void EnableHysteresis(bool enable_hysteresis)                = 0;
+    virtual void SetAsActiveLow(bool set_as_active_low)                  = 0;
+    virtual void SetAsAnalogMode(bool set_as_analog)                     = 0;
+    virtual void EnableDigitalFilter(bool enable_digital_filter)         = 0;
+    virtual void EnableFastMode(bool enable_fast_mode)                   = 0;
+    virtual void EnableI2cHighSpeedMode(bool enable_i2c_high_speed_mode) = 0;
+    virtual void EnableI2cHighCurrentDrive(
+        bool enable_i2c_high_current_drive)             = 0;
+    virtual void SetAsOpenDrain(bool set_as_open_drain) = 0;
+    virtual void EnableDac(bool enable_dac)             = 0;
 };
 
 class PinConfigure : public PinConfigureInterface
@@ -36,12 +42,17 @@ class PinConfigure : public PinConfigureInterface
     // Source: "UM10562 LPC408x/407x User manual" table 83 page 132
     enum PinBitMap : uint8_t
     {
-        kFunction    = 0,
-        kMode        = 3,
-        kHysteresis  = 5,
-        kInputInvert = 6,
-        kSlew        = 9,
-        kOpenDrain   = 10
+        kFunction            = 0,
+        kMode                = 3,
+        kHysteresis          = 5,
+        kInputInvert         = 6,
+        kAnalogDigitalMode   = 7,
+        kDigitalFilter       = 8,
+        kI2cHighSpeed        = 8,
+        kSlew                = 9,
+        kI2cHighCurrentDrive = 9,
+        kOpenDrain           = 10,
+        kDacEnable           = 16
     };
     SJ2_PACKED(struct) PinMap_t
     {
@@ -59,11 +70,13 @@ class PinConfigure : public PinConfigureInterface
         static_assert(port <= 5, "Port must be between 0 and 5");
         static_assert(pin <= 31, "Pin must be between 0 and 31");
         static_assert(port == 5 && pin <= 4,
-            "For port 5, the pin number must be equal to or below 4");
+                      "For port 5, the pin number must be equal to or below 4");
         return PinConfigure(port, pin);
     }
     constexpr PinConfigure(const uint8_t _port, const uint8_t _pin)
-        : port(_port), pin(_pin) { }
+        : port(_port), pin(_pin)
+    {
+    }
     void SetPinFunction(uint8_t function) final override
     {
         pin_map->_register[port][pin] =
@@ -87,11 +100,40 @@ class PinConfigure : public PinConfigureInterface
             BitPlace(pin_map->_register[port][pin], PinBitMap::kInputInvert,
                      set_as_active_low, 1);
     }
+    // Set bit to 0 to enable analog mode
+    void SetAsAnalogMode(bool set_as_analog = true) final override
+    {
+        pin_map->_register[port][pin] =
+            BitPlace(pin_map->_register[port][pin],
+                     PinBitMap::kAnalogDigitalMode, !set_as_analog, 1);
+    }
+    // Enable by setting bit to 0 to enable digital filter.
+    void EnableDigitalFilter(bool enable_digital_filter = true) final override
+    {
+        pin_map->_register[port][pin] =
+            BitPlace(pin_map->_register[port][pin], PinBitMap::kDigitalFilter,
+                     !enable_digital_filter, 1);
+    }
     void EnableFastMode(bool enable_fast_mode = true) final override
     {
         pin_map->_register[port][pin] =
             BitPlace(pin_map->_register[port][pin], PinBitMap::kSlew,
                      enable_fast_mode, 1);
+    }
+    // Enable by setting bit to 0 for i2c high speed mode
+    void EnableI2cHighSpeedMode(
+        bool enable_i2c_high_speed_mode = true) final override
+    {
+        pin_map->_register[port][pin] =
+            BitPlace(pin_map->_register[port][pin], PinBitMap::kI2cHighSpeed,
+                     !enable_i2c_high_speed_mode, 1);
+    }
+    void EnableI2cHighCurrentDrive(
+        bool enable_i2c_high_current_drive = true) final override
+    {
+        pin_map->_register[port][pin] = BitPlace(
+            pin_map->_register[port][pin], PinBitMap::kI2cHighCurrentDrive,
+            enable_i2c_high_current_drive, 1);
     }
     void SetAsOpenDrain(bool set_as_open_drain = true) final override
     {
@@ -99,17 +141,31 @@ class PinConfigure : public PinConfigureInterface
             BitPlace(pin_map->_register[port][pin], PinBitMap::kOpenDrain,
                      set_as_open_drain, 1);
     }
+    void EnableDac(bool enable_dac = true) final override
+    {
+        pin_map->_register[port][pin] =
+            BitPlace(pin_map->_register[port][pin], PinBitMap::kDacEnable,
+                     enable_dac, 1);
+    }
     inline uint32_t BitPlace(uint32_t target, uint32_t position, uint32_t value,
-                      uint32_t value_width)
+                             uint32_t value_width)
     {
         // Generate mask with all 1s
         uint32_t mask = 0xFFFFFFFF >> (32 - value_width);
         target &= ~(mask << position);
-        target |= value << position;
+        target |= (value & mask) << position;
         return target;
     }
+    uint8_t getPort()
+    {
+        return port;
+    }
+    uint8_t setPin()
+    {
+        return pin;
+    }
 
-   private:
+   protected:
     const uint8_t port;
     const uint8_t pin;
 };
