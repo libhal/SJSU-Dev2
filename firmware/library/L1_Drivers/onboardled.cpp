2f8770873899c1e509dd7b03b2be0c10af22673d
Adam Sanchez <atozadam@gmail.com>
2018-07-15 17:33:38 -0700

    Initial Implementation

---
+++ firmware/library/L1_Drivers/onboardled.cpp
@@ -0,0 +1,3 @@
+#include "L1_Drivers/onboardled.hpp"
+
+// work in progress
---
+++ firmware/library/L1_Drivers/onboardled_test.cpp
@@ -0,0 +1,7 @@
+#include "L0_LowLevel/LPC40xx.h"
+#include "L1_Drivers/onboardled.hpp"
+#include "L5_Testing/testing_frameworks.hpp"
+
+TEST_CASE("Testing Onboard LEDs", "[onboardled]")
+{
+}
