5988596bfd8f6e51aa162845ccd1e0b66040389b
Lawrence Wan <wanlawrence1@gmail.com>
2018-07-19 15:54:24 -0700

    Revison of Button API Interface
    Revised Interface of the button driver.
    - Combined invert button signal methods into one.
    - Changed GetButtonState() to Read()

---
+++ firmware/library/L1_Drivers/button_driver.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+class ButtonInterface {
+ public:
+         virtual void Init()                = 0;
+         virtual bool Read()                = 0;
+         virtual bool Released()            = 0;
+         virtual bool Pressed()             = 0;
+         virtual void InvertButtonSignal(bool enable_invert_signal = true) = 0;
+};
