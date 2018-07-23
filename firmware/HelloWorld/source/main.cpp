8758c151e34d124a04696851eeb9537abd01e3c6
Khalil Estell <khalil.estell@sjsu.edu>
2018-07-23 10:36:45 -0700

    Adding newline character to DEBUG_PRINT
    In order to make the DEBUG_PRINT look cleaner, a newline character is
    added to each DEBUG_PRINT.
    
    Removed extra newline characters from DEBUG_PRINT messages.
    
    Cleaning up some commented out code in PinConfigure.

--- firmware/HelloWorld/source/main.cpp
+++ firmware/HelloWorld/source/main.cpp
@@ -15,17 +15,17 @@ int main(void)
     fputs(SJ2_BACKGROUND_GREEN
          "================================== SJTwo Booted! "
          "==================================\n" SJ2_COLOR_RESET, stdout);
-    DEBUG_PRINT("Initializing LEDs...\n");
+    DEBUG_PRINT("Initializing LEDs...");
     LPC_IOCON->P1_1 &= ~(0b111);
     LPC_IOCON->P1_8 &= ~(0b111);
     LPC_GPIO1->DIR |= (1 << 1);
     LPC_GPIO1->PIN &= ~(1 << 1);
     LPC_GPIO1->DIR |= (1 << 8);
     LPC_GPIO1->PIN |= (1 << 8);
-    DEBUG_PRINT("LEDs Initialized...\n");
+    DEBUG_PRINT("LEDs Initialized...");
     fputs("Enter wait cycles for led animation: ", stdout);
     scanf("%lu", &cycles);
-    DEBUG_PRINT("Toggling LEDs...\n");
+    DEBUG_PRINT("Toggling LEDs...");
 
     while (1)
     {
