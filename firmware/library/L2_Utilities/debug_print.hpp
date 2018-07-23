0650c816fac5195fc424a4e415a7ce9e5da3d09f
Khalil Estell <khalil.estell@sjsu.edu>
2018-07-23 10:36:45 -0700

    Adding newline character to DEBUG_PRINT
    In order to make the DEBUG_PRINT look cleaner, a newline character is
    added to each DEBUG_PRINT.
    
    Removed extra newline characters from DEBUG_PRINT messages.
    
    Cleaning up some commented out code in PinConfigure.

--- firmware/library/L2_Utilities/debug_print.hpp
+++ firmware/library/L2_Utilities/debug_print.hpp
@@ -3,7 +3,7 @@
 #include <cstdio>
 #include "L2_Utilities/ansi_terminal_codes.hpp"
 // Printf style logging with filename, function name, and line number
-#define DEBUG_PRINT(format, ...)                                \
-    printf(SJ2_HI_BLUE "%s:" SJ2_HI_GREEN "%s:" SJ2_HI_YELLOW   \
-                       "%d> " SJ2_WHITE format SJ2_COLOR_RESET, \
+#define DEBUG_PRINT(format, ...)                                     \
+    printf(SJ2_HI_BLUE "%s:" SJ2_HI_GREEN "%s:" SJ2_HI_YELLOW        \
+                       "%d> " SJ2_WHITE format SJ2_COLOR_RESET "\n", \
            __FILE__, __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
