8a643d894624037df12b895027edcc522833544b
Conrad Bormann <Cbormann@live.com>
2018-07-23 20:28:08 -0700

    DAC Interface API

---
+++ firmware/library/L1_Drivers/dac.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+class DacInterface
+{
+ public:
+    Dac(void);
+    virtual bool Initialize();
+    virtual bool WriteDac(uint16_t dac_output);
+    virtual bool SetVoltage(float voltage);
+    // Sine Demonstration Function
+    virtual void SineDac(uint32_t frequency_hz);
+    // Sawtooth Demonstration Function
+    virtual void SawtoothDac(uint32_t frequency_hz);
+    // Triangle Demonstration Function
+    virtual void TriangleDac(uint32_t frequency_hz);
+    virtual void SetBias(bool bias_level);
+};
