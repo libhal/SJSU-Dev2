31e74187be01f57c9846ac14fb0b5b876865b17c
Khalil Estell <khalil.estell@sjsu.edu>
2018-07-23 17:26:35 -0700

    Integrating FreeRTOS
    Swapped out empty SVCall, and PendSV handlers with FreeRTOS OS calls.
    
    Added implementation of HardFault_Handler along with a
    GetRegistersFromStack, which will dump the offending call's pc, lr, and
    other registers.
    
    In Makefile, changed the include for FreeRTOS and third_party software
    to -isystem vs -I to mark them all as System headers, which will supress
    all warnings from them.
    
    Also, seperated C++ warnings and optimizations from the common warning
    an optimization flags, to remove warnings when compiling C files.

--- firmware/library/L0_LowLevel/startup.hpp
+++ firmware/library/L0_LowLevel/startup.hpp
@@ -3,3 +3,7 @@
 #include "L1_Drivers/system_timer.hpp"
 
 extern SystemTimer system_timer;
+
+extern "C" void xPortPendSVHandler(void);
+extern "C" void vPortSVCHandler(void);
+extern "C" void xPortSysTickHandler(void);
