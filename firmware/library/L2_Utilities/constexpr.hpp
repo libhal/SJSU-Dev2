c00be3d199284f86681d7c108e8be76b14c232ff
Khalil Estell <khalil.estell@sjsu.edu>
2018-08-05 15:47:20 -0700

    Reducing debug_print filename to basename
    Also added a constexpr.hpp file that will hold utility constexpr
    functions.

---
+++ firmware/library/L2_Utilities/constexpr.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <cstdlib>
+
+constexpr size_t StringLength(const char * str)
+{
+    size_t result = 0;
+    for (result = 0; str[result] != '\0'; result++)
+    {
+        continue;
+    }
+    return result + 1;
+}
+
+constexpr size_t BasenameLength(const char * path)
+{
+    size_t forward_slash = 0;
+    size_t length        = StringLength(path);
+    for (forward_slash = 0; forward_slash < length; forward_slash++)
+    {
+        if (path[(length - 1) - forward_slash] == '/')
+        {
+            break;
+        }
+    }
+    return forward_slash;
+}
--- firmware/library/L2_Utilities/debug_print.hpp
+++ firmware/library/L2_Utilities/debug_print.hpp
@@ -1,9 +1,35 @@
-// All
 #pragma once
 #include <cstdio>
 #include "L2_Utilities/ansi_terminal_codes.hpp"
+#include "L2_Utilities/constexpr.hpp"
+
+// Constructing this structure with a file path, will generate a structure with
+// a character array containing just the bare basename.
+template <size_t kPathLength, size_t kBasenameLength>
+struct FileBasename_t
+{
+    constexpr explicit FileBasename_t(const char (&path)[kPathLength])
+        : basename{}
+    {
+        size_t base_position = 0;
+        for (size_t i = kPathLength - kBasenameLength; i < kPathLength; i++)
+        {
+            basename[base_position] = path[i];
+            base_position++;
+        }
+        basename[base_position - 1] = '\0';
+    }
+    char basename[kBasenameLength];
+};
+
 // Printf style logging with filename, function name, and line number
-#define DEBUG_PRINT(format, ...)                                     \
-    printf(SJ2_HI_BLUE "%s:" SJ2_HI_GREEN "%s:" SJ2_HI_YELLOW        \
-                       "%d> " SJ2_WHITE format SJ2_COLOR_RESET "\n", \
-           __FILE__, __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+#define DEBUG_PRINT(format, ...)                                             \
+    do                                                                       \
+    {                                                                        \
+        static constexpr FileBasename_t<StringLength(__FILE__),              \
+                                        BasenameLength(__FILE__)>            \
+            file(__FILE__);                                                  \
+        printf(SJ2_HI_BLUE "%s:" SJ2_HI_GREEN "%s:" SJ2_HI_YELLOW            \
+                           "%d> " SJ2_WHITE format SJ2_COLOR_RESET "\n",     \
+               file.basename, __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__); \
+    } while (0)
