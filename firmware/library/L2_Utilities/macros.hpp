88fd25efcc59fcd33a5718760fd7a9fa062745a7
Khalil Estell <khalil.estell@sjsu.edu>
2018-10-24 21:45:00 -0700

    Updated doxygen config and generated documentation

--- firmware/library/L2_Utilities/macros.hpp
+++ firmware/library/L2_Utilities/macros.hpp
@@ -1,5 +1,8 @@
-// This file is meant for general purpose macros that can be used across the
-// SJSU-Dev2 environment.
+/// @ingroup SJSU-Dev2
+/// @defgroup Macros Utility Macros
+/// @brief This module is meant for general purpose macros that can be used
+/// across the SJSU-Dev2 environment.
+/// @{
 #pragma once
 #include "config.hpp"
 // SJ2_FUNCTION_INLINE will cause a function to be inlined at the call site.
@@ -17,41 +20,41 @@
 #else
 #define SJ2_SECTION(section_name) __attribute__((used, section(section_name)))
 #endif
-// SJ2_USED will use void casting as a means to convince the compiler that the
-// variable has been used in the software, to remove compiler warnings about
-// unused variables.
-// NOTE: this will not stop the compiler from optimizing this variable out.
+/// SJ2_USED will use void casting as a means to convince the compiler that the
+/// variable has been used in the software, to remove compiler warnings about
+/// unused variables.
+/// NOTE: this will not stop the compiler from optimizing this variable out.
 #define SJ2_USED(variable) ((void)variable)
-// SJ2_VARIADIC_USED can suppress unused parameter warnings within macros when
-// supplied with __VA_ARGS__.
+/// SJ2_VARIADIC_USED can suppress unused parameter warnings within macros when
+/// supplied with __VA_ARGS__.
 inline void UsedVariadicFunction(...) {}
 #define SJ2_VARIADIC_USED(...)         \
   if (false)                           \
   {                                    \
     UsedVariadicFunction(__VA_ARGS__); \
   }
-// These macros are used to stringify define values. For example:
-//
-//      #define VALUE true
-//      #define STRING "value = " STRINGIFY(s) => "value = true"
-//
+/// These macros are used to stringify define values. For example:
+///
+///      #define VALUE true
+///      #define STRING "value = " STRINGIFY(s) => "value = true"
+///
 #define SJ2_STRINGIFY(s) SJ2_STRINGIFY2(s)
 #define SJ2_STRINGIFY2(s) #s
-// Returns the length of an array
+/// Returns the length of an array
 #define SJ2_ARRAY_LENGTH(array) sizeof(array) / sizeof(*array)
-// SJ2_PACKED give a specified type a packed attribute
+/// SJ2_PACKED give a specified type a packed attribute
 #define SJ2_PACKED(type) type __attribute__((packed))
 // SJ2_IGNORE_STACK_TRACE will remove function profiling for this
 // specific function which means it will not be recoreded in the stack_trace
 // array and will not show up when a SJ2_DUMP_BACKTRACE() is called.
 #define SJ2_IGNORE_STACK_TRACE(function) \
   function __attribute__((no_instrument_function))
-// Set a function as a "weak" function. This means that if there is another
-// declaration of this exact function somewhere else in the software, the
-// non-weak function will be used instead of the weak function.
+/// Set a function as a "weak" function. This means that if there is another
+/// declaration of this exact function somewhere else in the software, the
+/// non-weak function will be used instead of the weak function.
 #define SJ2_WEAK __attribute__((weak))
-// Similar to the weak attribute, but also gives each function the
-// implementation of the function f.
+/// Similar to the weak attribute, but also gives each function the
+/// implementation of the function f.
 #if defined(__APPLE__)
 #define SJ2_ALIAS(f) \
   {                  \
