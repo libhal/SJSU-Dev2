ea46eaefdda12fe36a55bd5821ba65a22ad69d54
jeremy <chaujeremy7@gmail.com>
2018-11-10 14:53:42 -0800

    Uart Example Refractor
    1. Made improvements to the explanations of the example code.
    2. Adding more explanations to the example code.
    3. Replaced DEBUG_PRINT to LOG_INFO as requested.
    4. Fixed master merging conflicts.

--- firmware/examples/Uart/source/main.cpp
+++ firmware/examples/Uart/source/main.cpp
@@ -9,30 +9,38 @@
 
 int main(void)
 {
-    // This example code uses a loopback test Please connect the relevant TxD
-    // and RxD pins together. For this example please connect the TxD2 and RxD2
-    // pins together.
-    Uart uart2(Uart::Channels::kUart2);
-    // Calls the initialize function and checks if the
-    // function fails due to wrong input.
-    uart2.Initialize(38400);
-    // While loop to constantly send the array of data
-    while (true)
+  LOG_INFO("Uart application starting.");
+  LOG_INFO(
+      "The aplication uses a loopback test. The test will be constantly "
+      "sending data between the Tx and Rx pins."
+      "Please connect pins P2.8 and P2.9 together using a wire for this "
+      "test.");
+  Uart uart2(Uart::Channels::kUart2);
+  LOG_INFO(
+      "Initializing UART port 2 with a baud rate of 38400 bits per second");
+  uart2.Initialize(38400);
+  while (true)
+  {
+    LOG_INFO(
+        "A char array containing the characters 'Hello World' will"
+        "now be sent from the Tx pin to the Rx pin");
+    char message[] = "Hello World!";
+    for (size_t i = 0; message[i] != '\0'; i++)
     {
-        char message[] = "Hello World!";
-        // This first loop sends all the data within the array
-        for (size_t i = 0; message[i] != '\0'; i++)
-        {
-          uart2.Send(message[i]);
-        }
-        // This second loop receives all the sent data and prints it out
-        for (size_t i = 0; i < std::size(message)-1; i++)
-        {
-            char receive = uart2.Receive();
-            putchar(receive);
-        }
-        putchar('\n');
-        Delay(1000);
+      // This first loop sends all the data within the array
+      LOG_INFO("Sending characters now.");
+      uart2.Send(message[i]);
     }
-    return 0;
+    LOG_INFO("Recieving characters that arrived at Rx pin and printing them");
+    // This second loop receives all the sent data and prints it out
+    for (size_t i = 0; i < std::size(message) - 1; i++)
+    {
+      char receive = uart2.Receive();
+      putchar(receive);
+    }
+
+    putchar('\n');
+    Delay(1000);
+  }
+  return 0;
 }
