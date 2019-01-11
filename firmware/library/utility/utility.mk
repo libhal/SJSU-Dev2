6a068d85640d832c4c99bcdebc99aeb2301f3d2d
Khalil Estell <kammce@google.com>
2019-01-11 13:08:53 -0800

    Adding map utility function with test
    Also added bit_test.hpp to the utility.mk list of tests
    Fixed a few .mk files that were still using the escape line trick for
    adding to the TESTS variable.

--- firmware/library/utility/utility.mk
+++ firmware/library/utility/utility.mk
@@ -1,7 +1,8 @@
 INCLUDES +=
 SYSTEM_INCLUDES +=
 SOURCES +=
-TESTS += \
-	$(LIB_DIR)/utility/test/constexpr_test.cpp \
-	$(LIB_DIR)/utility/test/debug_test.cpp \
-	$(LIB_DIR)/utility/test/enum_test.cpp
+TESTS += $(LIB_DIR)/utility/test/constexpr_test.cpp
+TESTS += $(LIB_DIR)/utility/test/debug_test.cpp
+TESTS += $(LIB_DIR)/utility/test/enum_test.cpp
+TESTS += $(LIB_DIR)/utility/test/bit_test.cpp
+TESTS += $(LIB_DIR)/utility/test/map_test.cpp
