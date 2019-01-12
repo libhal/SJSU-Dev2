10acd555de963928ee4e2de1a6d332fcae07c56e
Khalil Estell <khalil.estell@sjsu.edu>
2019-01-12 12:34:29 -0800

    Adding "make user-test"
    Now you can test a select number of test files, rather than having to
    compile all of the test files, you can select use a project.mk file
    within your projects folder to include only the set of test files you
    want to comple.

---
+++ firmware/HelloWorld/project.mk
@@ -0,0 +1,7 @@
+# This file is meant to show how you can add your own set of tests to the list
+# of user tests when you invoke "make user-test"
+# If you require a source .cpp file to be linked into the test executable as
+# well, add it to the USER_TESTS list as well.
+USER_TESTS += $(LIB_DIR)/utility/test/bit_test.cpp
+USER_TESTS += $(LIB_DIR)/library/L1_Drivers/test/pin_test.cpp
+USER_TESTS += $(LIB_DIR)/library/L2_HAL/test/button_test.cpp
--- firmware/library/L4_Testing/L4.mk
+++ firmware/library/L4_Testing/L4.mk
@@ -4,3 +4,6 @@ SOURCES +=
 
 TESTS += $(LIB_DIR)/L4_Testing/freertos_mocks.cpp
 TESTS += $(LIB_DIR)/L4_Testing/main_test.cpp
+
+USER_TESTS += $(LIB_DIR)/L4_Testing/freertos_mocks.cpp
+USER_TESTS += $(LIB_DIR)/L4_Testing/main_test.cpp
