f5432a855b30890597e60376a0dbb9998b9f2104
Rabeel <rabeelio@github.com>
2019-01-15 21:22:53 -0800

    Initial Commit For CAN Driver Implementation

--- firmware/library/L1_Drivers/test/can_test.cpp
+++ firmware/library/L1_Drivers/test/can_test.cpp
@@ -1,5 +1,6 @@
 #include "L0_LowLevel/LPC40xx.h"
-#include "L1_Drivers/can.hpp"
+// TODO(#357): Fake queue.h functions in rtos.hpp
+// #include "L1_Drivers/can.hpp"
 #include "L4_Testing/testing_frameworks.hpp"
 
 // Uncomment this when can class has been created
