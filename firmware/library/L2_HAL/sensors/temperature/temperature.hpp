2bb1ce19020917651c67a3458515c3623adf77ea
chuefengvang <chuefeng.vang@sjsu.edu>
2019-01-17 17:51:45 -0800

    Add Temperature Sensor Interface.

--- firmware/library/L2_HAL/sensors/temperature/temperature.hpp
+++ firmware/library/L2_HAL/sensors/temperature/temperature.hpp
@@ -1,11 +1,12 @@
-// Interface used to sense the temperature of the temperature sensor.
 #pragma once
-#include <cstdint>
+
+#include "L1_Drivers/i2c.hpp"
+
 class TemperatureInterface
 {
  public:
-  virtual bool Initialize() = 0;
-  virtual uint32_t GetTemperatureBytes() = 0;
-  virtual float GetCelsius() = 0;
+  virtual bool Initialize()     = 0;
+  virtual float GetCelsius()    = 0;
   virtual float GetFahrenheit() = 0;
 };
+
