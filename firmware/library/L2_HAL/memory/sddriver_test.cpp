05b251d05b87ce59ac7a002e1c903b231b5a1035
Rolando Javier <rjavier441@gmail.com>
2019-01-22 23:58:46 -0800

    iImplementation of Sd Card Driver
    Added Sd Card Driver with Sd Card example. This commit
    integrates the initial working version of the Sd Card
    driver (includes block read/write/delete and sd card
    hardware initialization/mounting), code changes to
    address presubmit checks, and code changes that address
    style conformity, suggested changes, and linter checks.
    This commit also resolves the following issues:
    
    Resolves #151
    Resolves #152
    Resolves #160
    Resolves #161
    Resolves #162

---
+++ firmware/library/L2_HAL/memory/sddriver_test.cpp
@@ -0,0 +1,10 @@
+// Tests for the SD Card Driver Class
+#include "L2_HAL/memory/sddriver.hpp"
+#include "L4_Testing/testing_frameworks.hpp"
+EMIT_ALL_METHODS(Sd);
+
+TEST_CASE("Testing SD Card Driver Class", "[sd card driver]")
+{
+    Mock<SspInterface> mock_cs;     // CS:      SSP Chip Select
+}
+
