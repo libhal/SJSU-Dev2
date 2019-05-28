6d7560c9ef2a13124053e4ce493ed53869b61311
Khalil Estell <khalil.estell@sjsu.edu>
2019-05-27 20:26:35 -0700

    Platform specification in demos/sjtwo project.mk
    Removed additional for loop in presubmit checks. May need to put it back
    specifically for the multiplatform demos.
    
    Resolves #478

--- demos/sjtwo/adc/project.mk
+++ demos/sjtwo/adc/project.mk
@@ -1,1 +1,2 @@
-USER_TESTS += $(LIBRARY_DIR)/L1_Peripheral/test/adc_test.cpp
+USER_TESTS += $(LIBRARY_DIR)/L1_Peripheral/lpc40xx/test/adc_test.cpp
+PLATFORM = lpc40xx
--- demos/sjtwo/pwm/project.mk
+++ demos/sjtwo/pwm/project.mk
@@ -1,5 +1,2 @@
-# This file is meant to show how you can add your own set of tests to the list
-# of user tests when you invoke "make user-test"
-# If you require a source .cpp file to be linked into the test executable as
-# well, add it to the USER_TESTS list as well.
-USER_TESTS += $(LIBRARY_DIR)/L1_Peripheral/test/pwm_test.cpp
+USER_TESTS += $(LIBRARY_DIR)/L1_Peripheral/lpc40xx/test/pwm_test.cpp
+PLATFORM = lpc40xx
