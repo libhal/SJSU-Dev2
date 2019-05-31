5bbf906bf10ad1fa8e537a5ac41422b6046303ce
Khalil Estell <kammcecorp@gmail.com>
2019-05-31 01:11:11 -0700

    Adding DWT Counter Peripheral
    - Added DWT class
    - Added DWT class test
    - Changed lpc40xx and lpc17xx platforms to use DWT as
      ThreadRuntimeCounter()
    - Removed/Freed timer0 from use in startup.cpp which allows the
      application developer to use all timer peripherals if desired.

---
+++ library/L1_Peripheral/cortex/dwt_counter.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "L0_Platform/lpc40xx/LPC40xx.h"
+
+using ::sjsu::lpc40xx::DWT_Type;
+using ::sjsu::lpc40xx::CoreDebug_Type;
+
+namespace sjsu
+{
+namespace cortex
+{
+class DwtCounter
+{
+ public:
+  static inline DWT_Type * dwt = DWT;
+  static inline CoreDebug_Type * core = CoreDebug;
+
+  void Initialize()
+  {
+    core->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+    dwt->CYCCNT  = 0;
+    dwt->CTRL   |= DWT_CTRL_CYCCNTENA_Msk;
+  }
+  uint32_t GetCount()
+  {
+    return dwt->CYCCNT;
+  }
+};
+}  // namespace cortex
+}  // namespace sjsu
