f409f5373ee218c0abf116c860bb7972097237e1
Khalil Estell <khalil.estell@sjsu.edu>
2019-06-07 07:28:39 -0700

    Adding test for fatfs utility

--- library/utility/utility.mk
+++ library/utility/utility.mk
@@ -1,8 +1,9 @@
 INCLUDES +=
 SYSTEM_INCLUDES +=
 SOURCES +=
+TESTS += $(LIBRARY_DIR)/utility/test/bit_test.cpp
 TESTS += $(LIBRARY_DIR)/utility/test/constexpr_test.cpp
 TESTS += $(LIBRARY_DIR)/utility/test/debug_test.cpp
 TESTS += $(LIBRARY_DIR)/utility/test/enum_test.cpp
-TESTS += $(LIBRARY_DIR)/utility/test/bit_test.cpp
+TESTS += $(LIBRARY_DIR)/utility/test/fatfs_test.cpp
 TESTS += $(LIBRARY_DIR)/utility/test/map_test.cpp
