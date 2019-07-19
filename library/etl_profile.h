d6972d889d91ca75d3093f9ca0da5ef85b220ebf
Khalil Estell <kammcecorp@gmail.com>
2019-07-19 11:01:52 -0700

    Add alert developer if setup is out of date
    A yellow warning is generated when the value in the setup_version.txt
    file and the version of that file that was written into the
    ~/.sjsu-dev2.mk location file differ from each other.
    
    Also updates the root for cpplint
    
    Resolves #625

--- library/etl_profile.h
+++ library/etl_profile.h
@@ -1,10 +1,7 @@
-// Using traditional include guard to follow etl standards
-#ifndef LIBRARY_ETL_PROFILE_H_
-#define LIBRARY_ETL_PROFILE_H_
+#pragma once
 
 #define ETL_CHECK_PUSH_POP
 #define ETL_LOG_ERRORS
 #define ETL_VERBOSE_ERRORS
 
 #include "third_party/etl/profiles/cpp17.h"
-#endif  // LIBRARY_ETL_PROFILE_H_
