ad18dff075e12dc3fdb5fc26f815ce7101c89777
Khalil Estell <kammcecorp@gmail.com>
2019-08-06 07:50:52 -0700

    Adding multiplatform support, guide page and demo
    Resolves #476

--- library/utility/build_info.hpp
+++ library/utility/build_info.hpp
@@ -4,6 +4,10 @@
 #define TARGET Application
 #endif
 
+#if !defined(PLATFORM)
+#define PLATFORM lpc40xx
+#endif
+
 namespace sjsu
 {
 namespace build
@@ -14,7 +18,14 @@ enum class Target
   HostTest,         // NOLINT
 };
 
-constexpr const Target kTarget = Target::TARGET;
+enum class Platform
+{
+  lpc17xx,  // NOLINT
+  lpc40xx,  // NOLINT
+};
+
+constexpr const Target kTarget     = Target::TARGET;
+constexpr const Platform kPlatform = Platform::PLATFORM;
 
 constexpr const char * Stringify(Target target)
 {
@@ -23,6 +34,18 @@ constexpr const char * Stringify(Target target)
   {
     case Target::Application: result = "application"; break;
     case Target::HostTest: result = "host test"; break;
+    default: break;
+  }
+  return result;
+}
+
+constexpr const char * Stringify(Platform platform)
+{
+  const char * result = "invalid";
+  switch (platform)
+  {
+    case Platform::lpc17xx: result = "lpc17xx"; break;
+    case Platform::lpc40xx: result = "lpc40xx"; break;
     default: break;
   }
   return result;
