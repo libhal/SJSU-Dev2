851e873ad3c172bd8d2e8e852aa41070dd2dda53
Charles MacDonald <charlesmacd@users.noreply.github.com>
2019-08-20 18:41:05 -0700

    Added test stub

---
+++ library/L1_Peripheral/lpc40xx/test/capture_test.cpp
@@ -0,0 +1,22 @@
+
+#include "L0_Platform/lpc40xx/LPC40xx.h"
+#include "L0_Platform/interrupt.hpp"
+#include "L1_Peripheral/timer.hpp"
+#include "L1_Peripheral/capture.hpp"
+#include "L1_Peripheral/lpc40xx/timer.hpp"
+#include "L1_Peripheral/lpc40xx/capture.hpp"
+#include "L4_Testing/testing_frameworks.hpp"
+
+namespace sjsu::lpc40xx
+{
+EMIT_ALL_METHODS(Capture);
+
+TEST_CASE("Testing lpc40xx capture", "[lpc40xx-capture]")
+{
+}  // namespace sjsu::lpc40xx
+
+
+
+
+
+
