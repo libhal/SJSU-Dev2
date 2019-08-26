38ce5fa04963876f824ab8b3a4252cfa505fa210
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-26 22:01:47 +0000

    Deploy kammce/SJSU-Dev2 to github.com/kammce/SJSU-Dev2.git:gh-pages

--- library/L0_Platform/lpc17xx/lpc17xx.mk
+++ library/L0_Platform/lpc17xx/lpc17xx.mk
@@ -14,8 +14,12 @@ $(eval $(call BUILD_LIRBARY,liblpc17xx,LIBRARY_LPC17XX))
 include $(LIBRARY_DIR)/L0_Platform/arm_cortex/m3/m3.mk
 
 platform-flash:
+	@echo
+	@echo
 	@bash -c "\
 	source $(TOOLS_DIR)/nxpprog/modules/bin/activate && \
 	python3 $(TOOLS_DIR)/nxpprog/nxpprog.py \
-	--oscfreq=12000000 --baud=230400 --control \
-	\"$(SJDEV)\" \"$(BINARY)\""
+	--binary=\"$(BINARY)\" --device=\"$(SJDEV)\" \
+	--osfreq=12000000 --baud=230400 --control"
+	@echo
+	@echo
