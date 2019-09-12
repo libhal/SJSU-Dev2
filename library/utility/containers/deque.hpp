7f9cb71805cf483c9ea89ddf88692c9936774f88
Khalil Estell <kammcecorp@gmail.com>
2019-09-12 08:04:03 -0700

    Fix Doxygen API documentation generation
    Resolves #851

--- library/utility/containers/deque.hpp
+++ library/utility/containers/deque.hpp
@@ -1,50 +1,3 @@
-// -----------------------------------------------------------------------------
-// Usage with std::deque:
-// -----------------------------------------------------------------------------
-//
-//   sjsu::FixedAllocator<int, 8 * std::__deque_buf_size(sizeof(int))>
-//           fixed_list_allocator;
-//   std::deque<int, decltype(fixed_list_allocator)>
-//           deque(fixed_list_allocator);
-//
-//    or
-//
-//    sjsu::Deque<int, 8> list(sjsu::Deque<int, 8>::allocator_type{});
-//
-// -----------------------------------------------------------------------------
-// Deque Usage Notes:
-// -----------------------------------------------------------------------------
-// The example list can store up to roughly 8 elements. Resize to your needs.
-// Deque allocates arrays in block chunks. When it allocates greatly depends on
-// how items were inserted or removed from the deque. Thus the memory allocated
-// by the allocator cannot be precise as it is with the other containers. You
-// may end up with less memory then needed or a lot of extra memory.
-
-// -----------------------------------------------------------------------------
-// Usage with std::deque:
-// -----------------------------------------------------------------------------
-//
-//   sjsu::FixedAllocator<int, 8 * std::__deque_buf_size(sizeof(int))>
-//           fixed_list_allocator;
-//   std::deque<int, decltype(fixed_list_allocator)>
-//           deque(fixed_list_allocator);
-//
-//    or
-//
-//    sjsu::Deque<int, 8> list(sjsu::Deque<int, 8>::allocator_type{});
-//
-// -----------------------------------------------------------------------------
-// Deque Usage Notes:
-// -----------------------------------------------------------------------------
-// The example list can store up to roughly 8 elements. Resize to your needs.
-// Deque allocates arrays in block chunks. When it allocates greatly depends on
-// how items were inserted or removed from the deque. Thus the memory allocated
-// by the allocator cannot be precise as it is with the other containers. You
-// may end up with less memory then needed, or a lot of extra memory.
-//
-// -----------------------------------------------------------------------------
-// All other containers have not yet been tested with the allocator.
-// -----------------------------------------------------------------------------
 #pragma once
 
 #include <deque>
@@ -62,6 +15,30 @@
 
 namespace sjsu
 {
+/// ----------------------------------------------------------------------------
+/// Using std::deque
+/// ----------------------------------------------------------------------------
+///
+/// ```
+/// sjsu::FixedAllocator<int, 8 * std::__deque_buf_size(sizeof(int))>
+///       fixed_list_allocator;
+/// std::deque<int, decltype(fixed_list_allocator)> deque(fixed_list_allocator);
+/// ```
+///
+///    or
+///
+/// ```
+///    sjsu::Deque<int, 8> list(sjsu::Deque<int, 8>::allocator_type{});
+/// ```
+///
+/// ----------------------------------------------------------------------------
+/// Deque Usage Notes
+/// ----------------------------------------------------------------------------
+/// The example list can store up to roughly 8 elements. Resize to your needs.
+/// Deque allocates arrays in block chunks. When it allocates greatly depends on
+/// how items were inserted or removed from the deque. Thus the memory allocated
+/// by the allocator cannot be precise as it is with the other containers. You
+/// may end up with less memory then needed or a lot of extra memory.
 template <class T, const size_t length>
 using Deque =
     std::deque<T, FixedAllocator<T, length * DEQUE_BUFFER_SIZE(T)>>;
