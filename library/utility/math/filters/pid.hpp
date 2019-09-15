cd0986bf87d8d108adbe5a8bbdcc3210871d5545
Pushpal Patil <gauripushp@gmail.com>
2019-09-15 10:05:42 -0700

    pid basic layout

---
+++ library/utility/math/filters/pid.hpp
@@ -0,0 +1,21 @@
+
+
+namespace filter
+{
+	class PID.filter
+	{
+		PID(int oneP, int twoI, int threeD)
+		{
+			//..
+		}
+
+		float calculate(float input, float microseconds_delta, RealTimeelta())
+		{
+			//..
+		}
+		float SetGoal(float goal)
+		{
+			//..
+		}
+	};
+}
