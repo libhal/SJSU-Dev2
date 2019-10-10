ad52bf5a062a8dec43b88e0c3e41f305551ca0f3
Khalil Estell <kammcecorp@gmail.com>
2019-10-10 12:39:41 -0700

    Integrate 3P tool GDB Dashboard
    With GDB Dashboard, debugging firmware using GDB on the commandline
    becomes an absolute breeze. Its is borderline unusable without this
    plugin, so each time you run GDB, the local file will be sourced and
    used.
    
    Resolves #894

--- tools/launch_openocd_gdb.sh
+++ tools/launch_openocd_gdb.sh
@@ -1,37 +1,41 @@
 #!/bin/bash
 
-OPENOCD=$1
-DEBUG_DEVICE=$2
-OPENOCD_CONFIG=$3
-DEVICE_GDB=$4
-EXECUTABLE=$5
+DEVICE_GDB=$1
+GDBINIT_PATH=$2
+PLATFORM=$3
+EXECUTABLE=$4
+OPENOCD=$5
+DEBUG_DEVICE=$6
+OPENOCD_CONFIG=$7
 
-## Uncomment the line below to get a bit of debug information
-# echo "$OPENOCD/bin/openocd  -s $OPENOCD/scripts/ \
-#-c \"source [find interface/$DEBUG_DEVICE.cfg]\" -f $OPENOCD_CONFIG &"
+GDB_ARGS=""
 
-$OPENOCD/bin/openocd  -s $OPENOCD/scripts/ \
--c "source [find interface/$DEBUG_DEVICE.cfg]" -f $OPENOCD_CONFIG &
-
-# Capture the pid of the background OpenOCD process
-OPENOCD_PID=$(echo $!)
-# Wait for OpenOCD to continue or quit
-sleep .25
-# Use kill to check if OpenOCD is running
-kill -0 $OPENOCD_PID &> /dev/null
-# Capture success or failure of check above
-OPENOCD_IS_RUNNING=$?
-if [ $OPENOCD_IS_RUNNING -ne 0 ]
+if [ $PLATFORM == "linux" ]
 then
-RED="\x1B[31;1m"
-RESET="\x1B[0m"
-echo
-echo -e "$RED OpenOCD failed to start, exiting $RESET"
-echo
-exit 1
+$DEVICE_GDB $EXECUTABLE -ex "source $GDBINIT_PATH"
+else # For all other platforms
+  $OPENOCD/bin/openocd  -s $OPENOCD/scripts/ \
+  -c "source [find interface/$DEBUG_DEVICE.cfg]" -f $OPENOCD_CONFIG &
+
+  # Capture the pid of the background OpenOCD process
+  OPENOCD_PID=$(echo $!)
+  # Wait for OpenOCD to continue or quit
+  sleep .25
+  # Use kill to check if OpenOCD is running
+  kill -0 $OPENOCD_PID &> /dev/null
+  # Capture success or failure of check above
+  OPENOCD_IS_RUNNING=$?
+  if [ $OPENOCD_IS_RUNNING -ne 0 ]
+  then
+    RED="\x1B[31;1m"
+    RESET="\x1B[0m"
+    echo
+    echo -e "$RED OpenOCD failed to start, exiting $RESET"
+    echo
+    exit 1
+  fi
+  GDB_ARGS="$GDB_ARGS -ex \"target remote :3333\""
+
+  echo "Killing OpenOCD PID: $OPENOCD_PID"
+  kill $OPENOCD_PID
 fi
-
-$DEVICE_GDB -ex "target remote :3333" $EXECUTABLE
-
-echo "Killing OpenOCD PID: $OPENOCD_PID"
-kill $OPENOCD_PID
