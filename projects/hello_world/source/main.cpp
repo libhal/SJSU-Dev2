6bd7707dd47b1a2159b462a2e6838cddc95c5a5f
Khalil Estell <kammcecorp@gmail.com>
2019-10-27 13:43:07 -0700

    Add Semihosting Printf to all ARM builds
    Semihosting printf will only occur if a debugger is attached to the
    device. Otherwise, the trace_write() function will simply return.
    
    Resolves #929

--- projects/hello_world/source/main.cpp
+++ projects/hello_world/source/main.cpp
@@ -2,18 +2,18 @@
 
 #include "utility/log.hpp"
 #include "utility/time.hpp"
+#include "third_party/semihost/trace.h"
 
 int main()
 {
-  LOG_INFO("Staring Hello World Application");
+  LOG_INFO("Starting Hello World Application");
   while (true)
   {
     for (uint8_t i = 0; i < 16; i++)
     {
       LOG_INFO("Hello World 0x%X", i);
-      sjsu::Delay(500ms);
+      sjsu::Delay(0ms);
     }
   }
-
   return -1;
 }
