2aeae99f17d84d4eaadb62bb35f8bea11bc58c60
Khalil Estell <kammcecorp@gmail.com>
2019-12-12 11:43:59 -0800

    Improved testing with Delay() in host tests
    - Add unit test for the time library.
    - Add documentation for time library.
    
    Resolves #953
    Resolves #770

--- library/utility/utility.mk
+++ library/utility/utility.mk
@@ -9,4 +9,5 @@ TESTS += $(LIBRARY_DIR)/utility/test/enum_test.cpp
 TESTS += $(LIBRARY_DIR)/utility/test/fatfs_test.cpp
 TESTS += $(LIBRARY_DIR)/utility/test/map_test.cpp
 TESTS += $(LIBRARY_DIR)/utility/test/stopwatch_test.cpp
+TESTS += $(LIBRARY_DIR)/utility/test/time_test.cpp
 TESTS += $(LIBRARY_DIR)/utility/math/test/average_test.cpp
