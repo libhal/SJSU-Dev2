c9cacf521a7c836e5b0fa9bff9d4b61defcff1ae
Khalil Estell <kammcecorp@gmail.com>
2020-01-15 17:17:44 -0800

    Add API docs for SJOne and SJTwo board files
    Resolves #756
    Resolves #759
    Resolves #814

--- library/L2_HAL/boards/sjtwo.hpp
+++ library/L2_HAL/boards/sjtwo.hpp
@@ -11,27 +11,39 @@
 
 #include "L3_Application/graphics.hpp"
 
+/// Namespace for SJTwo board peripheral and device definitions
 struct sjtwo // NOLINT
 {
+  /// Predefined SJTwo spi0 peripheral
   inline static sjsu::lpc40xx::Spi spi0 =
       sjsu::lpc40xx::Spi(sjsu::lpc40xx::Spi::Bus::kSpi0);
+  /// Predefined SJTwo spi1 peripheral
   inline static sjsu::lpc40xx::Spi spi1 =
       sjsu::lpc40xx::Spi(sjsu::lpc40xx::Spi::Bus::kSpi1);
+  /// Predefined SJTwo spi2 peripheral
   inline static sjsu::lpc40xx::Spi spi2 =
       sjsu::lpc40xx::Spi(sjsu::lpc40xx::Spi::Bus::kSpi2);
 
+  /// Predefined SJTwo i2c0 peripheral
   inline static sjsu::lpc40xx::I2c i2c0 =
       sjsu::lpc40xx::I2c(sjsu::lpc40xx::I2c::Bus::kI2c0);
+  /// Predefined SJTwo i2c1 peripheral
   inline static sjsu::lpc40xx::I2c i2c1 =
       sjsu::lpc40xx::I2c(sjsu::lpc40xx::I2c::Bus::kI2c1);
+  /// Predefined SJTwo i2c2 peripheral
   inline static sjsu::lpc40xx::I2c i2c2 =
       sjsu::lpc40xx::I2c(sjsu::lpc40xx::I2c::Bus::kI2c2);
 
+  /// Predefined SJTwo led0 peripheral
   inline static sjsu::lpc40xx::Gpio led0 = sjsu::lpc40xx::Gpio(2, 3);
+  /// Predefined SJTwo led1 peripheral
   inline static sjsu::lpc40xx::Gpio led1 = sjsu::lpc40xx::Gpio(1, 26);
+  /// Predefined SJTwo led2 peripheral
   inline static sjsu::lpc40xx::Gpio led2 = sjsu::lpc40xx::Gpio(1, 24);
+  /// Predefined SJTwo led3 peripheral
   inline static sjsu::lpc40xx::Gpio led3 = sjsu::lpc40xx::Gpio(1, 18);
 
+  /// @returns fully constructed onboard Oled device driver
   [[gnu::always_inline]] inline static sjsu::Graphics & Oled()
   {
     static sjsu::lpc40xx::Gpio oled_cs = sjsu::lpc40xx::Gpio(1, 22);
@@ -41,12 +53,14 @@ struct sjtwo // NOLINT
     return oled;
   }
 
+  /// @returns fully constructed onboard Accelerometer device driver
   [[gnu::always_inline]] inline static sjsu::Mma8452q & Accelerometer()
   {
     static sjsu::Mma8452q accelerometer(i2c2);
     return accelerometer;
   }
 
+  /// @returns fully constructed onboard Sd Card device driver
   [[gnu::always_inline]] inline static sjsu::Sd & SdCard()
   {
     static sjsu::lpc40xx::Gpio sd_cs = sjsu::lpc40xx::Gpio(1, 8);
@@ -54,12 +68,14 @@ struct sjtwo // NOLINT
     return sd;
   }
 
+  /// @returns fully constructed onboard Gesture device driver
   [[gnu::always_inline]] inline static sjsu::Apds9960 & Gesture()
   {
     static sjsu::Apds9960 apds9960(i2c2);
     return apds9960;
   }
 
+  /// @returns fully constructed onboard Temperature device driver
   [[gnu::always_inline]] inline static sjsu::Si7060 & Temperature()
   {
     static sjsu::Si7060 si7060(i2c2);
