c1ad45d0090fbd54a968d510917817709a9c0cbe
Khalil Estell <kammcecorp@gmail.com>
2020-01-30 18:45:29 -0800

    Esp8266 consistently working
    Methods using timeouts now expire properly.
    
    Introducing the TimeoutTimer which takes a timeout and can return the
    amount of time left since the TimeoutTimer's creation based on the
    systems Uptime() function.
    
    Read no longer waits for multiple +IPD packets. It will simply return
    the results of the first +IPD packet it encounters. For HTTP requests,
    "Content-Length" should be checked to see if further packets will need
    to be read from the stream.

--- demos/sjtwo/esp8266/source/main.cpp
+++ demos/sjtwo/esp8266/source/main.cpp
@@ -7,18 +7,11 @@
 #include "utility/log.hpp"
 #include "utility/debug.hpp"
 
-constexpr char kGetRequestExample[] =
+constexpr std::string_view kHost = "www.example.com";
+constexpr std::string_view kGetRequestExample =
     "GET / HTTP/1.1\r\n"
-    "Host: www.example.com\r\n\r\n";
-
-#define LOG_IF_NOT_SUCCESSFUL(expression)                     \
-  {                                                           \
-    sjsu::Status log_no_success_status = (expression);        \
-    if (log_no_success_status != sjsu::Status::kSuccess)      \
-    {                                                         \
-      sjsu::LogWarning("Expression Failed: %s", #expression); \
-    }                                                         \
-  }
+    "Host: www.example.com\r\n"
+    "\r\n";
 
 int main()
 {
@@ -26,47 +19,41 @@ int main()
   sjsu::lpc40xx::Uart uart3(sjsu::lpc40xx::Uart::Port::kUart3);
   sjsu::Esp8266 wifi(uart3);
 
-  sjsu::LogInfo("Initializing Wifi...");
-  LOG_IF_NOT_SUCCESSFUL(wifi.Initialize());
+  sjsu::LogInfo("Initializing Esp8266 module...");
+  LOG_ON_FAILURE(wifi.Initialize());
 
   while (true)
   {
-    sjsu::LogInfo("Attempting to connect...");
-    auto status = wifi.ConnectToAccessPoint("KAMMCE-PHONE", "roverteam");
+    sjsu::LogInfo("Connecting to WiFi...");
+    auto status = wifi.ConnectToAccessPoint("wifi-ssid", "password");
     if (status == sjsu::Status::kSuccess)
     {
       break;
     }
-    sjsu::LogInfo("Failed to connect... Retrying ...");
+    sjsu::LogWarning("Failed to connect to WiFi... Retrying ...");
     wifi.DisconnectFromAccessPoint();
   }
 
-  sjsu::LogInfo("Connected to Wifi");
+  sjsu::LogInfo("Connected to WiFi!!");
 
-  for (int i = 0; i < 2; i++)
-  {
-    sjsu::LogInfo("Connecting to server...");
-    LOG_IF_NOT_SUCCESSFUL(wifi.Connect(
-        sjsu::InternetSocket::Protocol::kTCP, "www.example.com", 80, 5s));
+  sjsu::LogInfo("Connecting to server (%s)...", kHost.data());
+  LOG_ON_FAILURE(
+      wifi.Connect(sjsu::InternetSocket::Protocol::kTCP, kHost, 9000, 5s));
 
-    sjsu::LogInfo("Send Write Request...");
-    LOG_IF_NOT_SUCCESSFUL(
-        wifi.Write(kGetRequestExample, sizeof(kGetRequestExample), 100ms));
+  LOG_ON_FAILURE(
+      wifi.Write(kGetRequestExample.data(), kGetRequestExample.size(), 5s));
 
-    sjsu::LogInfo("Read Back Response...");
-    char response[1024 * 4] = {};
-    size_t read_back        = wifi.Read(response, sizeof(response), 10s);
+  std::array<char, 2048> response;
+  size_t read_back = 0;
+  read_back += wifi.Read(&response[read_back], response.size(), 10s);
+  read_back +=
+      wifi.Read(&response[read_back], response.size() - read_back, 10s);
 
-    sjsu::LogInfo("Hexdumping Response...");
-    sjsu::debug::Hexdump(response, read_back);
+  sjsu::LogInfo("Printing Server Response:");
+  printf("%.*s\n", read_back, response.data());
+  puts("===================================================================");
 
-    sjsu::LogInfo("Direct Print...");
-    printf("%.*s\r", read_back, response);
-
-    wifi.Close();
-    sjsu::Delay(5s);
-  }
-
+  wifi.Close();
   wifi.DisconnectFromAccessPoint();
 
   return 0;
