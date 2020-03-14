7f0bbf5bf698ae7fd9cbbc8a0fd007bfcc3dd7d3
Khalil Estell <kammcecorp@gmail.com>
2020-03-14 16:17:48 -0700

    Add Watchdog Documentation
    Resolves #825
    Resolves #784

--- library/L1_Peripheral/lpc40xx/watchdog.hpp
+++ library/L1_Peripheral/lpc40xx/watchdog.hpp
@@ -15,22 +15,29 @@ namespace sjsu
 {
 namespace lpc40xx
 {
+/// Watchdog driver for the lpc40xx
 class Watchdog final : public sjsu::Watchdog
 {
  public:
-  const uint8_t kWatchdogResetAndEnable = 0x3;
-  const uint32_t kTimerWarningMax       = 0b11'1111'1111;
-  static constexpr units::frequency::hertz_t kWatchdogClockFrequency = 500_kHz;
-  static constexpr units::frequency::hertz_t kWatchdogClockDivider   = 4_Hz;
-  inline static LPC_WDT_TypeDef * wdt_base                           = LPC_WDT;
+  /// Pointer to the watchdog peripheral
+  inline static LPC_WDT_TypeDef * wdt_base = lpc40xx::LPC_WDT;
 
-  explicit Watchdog(uint8_t priority = -1) : priority_(priority) {}
+  /// Watchdog constructor.
+  ///
+  /// @param interrupt_priority - interrupt priority
+  explicit Watchdog(uint8_t interrupt_priority = -1)
+      : priority_(interrupt_priority)
+  {
+  }
 
-  // Initializes the watchdog timer
   Status Initialize(std::chrono::seconds duration) const override
   {
+    constexpr units::frequency::hertz_t kWatchdogClockDivider   = 4_Hz;
+    constexpr units::frequency::hertz_t kWatchdogClockFrequency = 500_kHz;
+
     uint32_t watchdog_clock_scalar =
         (kWatchdogClockFrequency / kWatchdogClockDivider).to<uint32_t>();
+
     uint32_t timer_constant =
         (watchdog_clock_scalar * static_cast<uint32_t>(duration.count()));
 
@@ -38,10 +45,12 @@ class Watchdog final : public sjsu::Watchdog
     wdt_base->TC = bit::Extract(timer_constant, { .position = 0, .width = 24 });
 
     // Enables the watchdog and enables the watchdog reset
-    wdt_base->MOD = kWatchdogResetAndEnable;
+    constexpr uint8_t kWatchdogResetAndEnable = 0x3;
+    wdt_base->MOD                             = kWatchdogResetAndEnable;
 
     // Insert timer_warning value into WARNINT register
-    wdt_base->WARNINT = kTimerWarningMax;
+    constexpr uint32_t kTimerWarningMax = 0b11'1111'1111;
+    wdt_base->WARNINT                   = kTimerWarningMax;
 
     return Status::kSuccess;
   }
@@ -56,18 +65,19 @@ class Watchdog final : public sjsu::Watchdog
     });
   }
 
-  // Feeds the watchdog timer
   void FeedSequence() const override
   {
     // Insert valid feed sequence
     wdt_base->FEED = 0xAA;
     wdt_base->FEED = 0x55;
   }
-  // Reads the current counter value of the watchdog timer
+
   uint32_t CheckTimeout() const override
   {
     return wdt_base->TV;
   }
+
+ private:
   uint8_t priority_;
 };
 }  // namespace lpc40xx
