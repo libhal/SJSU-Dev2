8b7dff3dca1cc258a1d17c6e0822754705adec50
Khalil Estell <kammcecorp@gmail.com>
2020-03-15 13:25:55 -0700

    Add Distance Sensor Docs
    Resolves #783
    Resolves #720

--- library/L2_HAL/sensors/distance/time_of_flight/tfmini.hpp
+++ library/L2_HAL/sensors/distance/time_of_flight/tfmini.hpp
@@ -17,51 +17,67 @@
 
 namespace sjsu
 {
+/// Distance sensor driver for the TFMini LiDAR module
+/// Find link here: https://www.sparkfun.com/products/14588
 class TFMini final : public DistanceSensor
 {
  public:
-  static constexpr uint32_t kBaudRate                = 115200;
-  static constexpr std::chrono::nanoseconds kTimeout = 1s;
+  /// Default timeout for reading responses from the TFMini LiDar module.
+  /// The device's test latency is 100Hz, which is 10ms per sample, thus a
+  /// timeout time of 3x that should give enough time to acquire the sample and
+  /// respond back via UART.
+  static constexpr std::chrono::nanoseconds kTimeout = 3 * 10ms;
 
-  static constexpr uint8_t kFrameHeader           = 0x59;
-  static constexpr uint8_t kLongDistMode          = 0x07;
-  static constexpr uint8_t kCommandLength         = 8;
-  static constexpr uint8_t kDeviceDataLength      = 9;
-  static constexpr uint8_t kThresholdByte         = 4;
+  /// Response packet frame header used for detecting the start of a proper
+  /// response packet.
+  static constexpr uint8_t kFrameHeader = 0x59;
+
+  /// Used to set the device to long range mode (currently not utilized)
+  static constexpr uint8_t kLongDistMode = 0x07;
+
+  /// Sensor signal strength lower bounds
   static constexpr uint8_t kStrengthLowerLimitCap = 80;
-  static constexpr uint32_t kStrengthUpperBound   = 3000;
 
+  /// Sensor signal strength upper bounds
+  static constexpr uint32_t kStrengthUpperBound = 3000;
+
+  /// Device response packet length
+  static constexpr uint8_t kDeviceDataLength = 9;
+
+  /// Device command packet length
+  static constexpr uint8_t kCommandLength = 8;
+
+  /// Command packet to enable configuration of device
   static constexpr uint8_t kConfigCommand[kCommandLength] = {
     0x42, 0x57, 0x02, 0x00, 0x00, 0x00, 0x01, 0x02,
   };
+
+  /// Command packet to disable configuration of device
   static constexpr uint8_t kExitConfigCommand[kCommandLength] = {
     0x42, 0x57, 0x02, 0x00, 0x00, 0x00, 0x00, 0x02
   };
-  static constexpr uint8_t kSetExternalTriggerMode[kCommandLength] = {
-    0x42, 0x57, 0x02, 0x00, 0x00, 0x00, 0x00, 0x40
-  };
-  static constexpr uint8_t kPromptMeasurementCommand[kCommandLength] = {
-    0x42, 0x57, 0x02, 0x00, 0x00, 0x00, 0x00, 0x41
-  };
+
+  /// Command packet to set device units to millimeters.
   static constexpr uint8_t kSetDistUnitMM[kCommandLength] = {
     0x42, 0x57, 0x02, 0x00, 0x00, 0x00, 0x00, 0x1A,
   };
-  static constexpr uint8_t kEchoSuccess[kCommandLength] = {
-    0x42, 0x57, 0x02, 0x01, 0x00, 0x00, 0x01, 0x02,
-  };
-  static constexpr uint8_t kConfigExit[kCommandLength] = {
-    0x42, 0x57, 0x02, 0x01, 0x00, 0x00, 0x00, 0x02,
-  };
-  static constexpr uint8_t kUpdateMinThresholdCommand[kCommandLength] = {
-    0x42, 0x57, 0x02, 0x00, 0xEE, 0x00, 0x00, 0x20
-  };
-  // TFMini firmware version 15X
-  static constexpr uint8_t kShortDistMode15 = 0x02;
-  // TFMini firmware version 16X
-  static constexpr uint8_t kShortDistMode16 = 0x00;
-  static constexpr uint8_t kMidDistMode16   = 0x03;
 
+  /// Command packet to allow distance capture on kPromptMeasurementCommand
+  /// command.
+  static constexpr uint8_t kSetExternalTriggerMode[kCommandLength] = {
+    0x42, 0x57, 0x02, 0x00, 0x00, 0x00, 0x00, 0x40
+  };
+
+  /// Command packet to request distance measurement data along with signal
+  /// strength data.
+  static constexpr uint8_t kPromptMeasurementCommand[kCommandLength] = {
+    0x42, 0x57, 0x02, 0x00, 0x00, 0x00, 0x00, 0x41
+  };
+
+  /// @param uart - UART peripheral connected to the RX and TX of the TFMini
+  ///               module.
   explicit constexpr TFMini(Uart & uart) : uart_pin_(uart) {}
+
   /// Initialize and enable hardware. This must be called before any other
   /// method in this interface is called.
   /// Sets the baud rate as well as configures the TFMini to have an external
@@ -71,7 +87,9 @@ class TFMini final : public DistanceSensor
   /// @returns Status::kSuccess if initialization succeeds
   Status Initialize() override
   {
-    Status init_success = uart_pin_.Initialize(kBaudRate);
+    constexpr uint32_t kBaudRate = 115200;
+    Status init_success          = uart_pin_.Initialize(kBaudRate);
+
     if (IsOk(init_success))
     {
       if (!SendCommandAndCheckEcho(kConfigCommand) ||
@@ -172,6 +190,10 @@ class TFMini final : public DistanceSensor
   /// @returns Status::kSuccess if device is successfully updated
   Status SetMinSignalThreshhold(uint8_t lower_threshold = 20)
   {
+    constexpr uint8_t kUpdateMinThresholdCommand[kCommandLength] = {
+      0x42, 0x57, 0x02, 0x00, 0xEE, 0x00, 0x00, 0x20
+    };
+    constexpr uint8_t kThresholdByte = 4;
     std::array<uint8_t, kCommandLength> updated_min_threshold_command;
 
     uint8_t low_limit = lower_threshold;
@@ -202,6 +224,13 @@ class TFMini final : public DistanceSensor
  private:
   bool SendCommandAndCheckEcho(const uint8_t * command) const
   {
+    constexpr uint8_t kEchoSuccess[kCommandLength] = {
+      0x42, 0x57, 0x02, 0x01, 0x00, 0x00, 0x01, 0x02,
+    };
+    constexpr uint8_t kConfigExit[kCommandLength] = {
+      0x42, 0x57, 0x02, 0x01, 0x00, 0x00, 0x00, 0x02,
+    };
+
     uint8_t echo[kCommandLength];
 
     uart_pin_.Write(command, kCommandLength);
