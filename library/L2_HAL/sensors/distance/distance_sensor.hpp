9b56e29a1e6bc329aebc8d8a34e671931b4f6bac
Khalil Estell <kammcecorp@gmail.com>
2020-03-15 13:25:55 -0700

    Add Distance Sensor Docs
    Resolves #783
    Resolves #720

--- library/L2_HAL/sensors/distance/distance_sensor.hpp
+++ library/L2_HAL/sensors/distance/distance_sensor.hpp
@@ -11,11 +11,26 @@
 
 namespace sjsu
 {
+/// Interface for a sensor that can measure distance in a single dimension, such
+/// as 1D lidar, ultrasonic range sensor, or infared distance sensor.
+/// @ingroup sensors
 class DistanceSensor
 {
  public:
+  /// Initialize distance sensor hardware. Must be called before running any
+  /// other methods.
   virtual Status Initialize() = 0;
+
+  /// Trigger a capture of the current distance reading and return it.
+  ///
+  /// @param distance - output parameter to contain the distance results.
   virtual Status GetDistance(units::length::millimeter_t * distance) = 0;
-  virtual Status GetSignalStrengthPercent(float * strength)          = 0;
+
+  /// Get the signal strength from the distance sensor.
+  ///
+  /// @param strength - output parameter to contain the strength of the system.
+  ///        The exact value
+  /// @return Status
+  virtual Status GetSignalStrengthPercent(float * strength) = 0;
 };
 }  // namespace sjsu
