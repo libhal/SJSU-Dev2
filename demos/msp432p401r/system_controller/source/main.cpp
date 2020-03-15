68b29c11cafedb212601fc26b75b5f9a3c7fccfd
David <huangd95@gmail.com>
2020-03-14 21:18:12 -0700

    Adding MSP432P401R System Controller

---
+++ demos/msp432p401r/system_controller/source/main.cpp
@@ -0,0 +1,84 @@
+#include <cstdio>
+
+#include "L0_Platform/msp432p401r/msp432p401r.h"
+#include "L1_Peripheral/cortex/system_timer.hpp"
+#include "L1_Peripheral/msp432p401r/system_controller.hpp"
+#include "utility/bit.hpp"
+#include "utility/log.hpp"
+#include "utility/time.hpp"
+
+namespace
+{
+void ConfigureClockOutPins()
+{
+  // Configure P4.2 to output ACLK
+  constexpr sjsu::bit::Mask kAclkBitMask = sjsu::bit::CreateMaskFromRange(2);
+  sjsu::msp432p401r::P4->SEL1 =
+      sjsu::bit::Clear(sjsu::msp432p401r::P4->SEL1, kAclkBitMask);
+  sjsu::msp432p401r::P4->SEL0 =
+      sjsu::bit::Set(sjsu::msp432p401r::P4->SEL0, kAclkBitMask);
+  sjsu::msp432p401r::P4->DIR =
+      sjsu::bit::Set(sjsu::msp432p401r::P4->DIR, kAclkBitMask);
+
+  // Configure P4.3 to output MCLK
+  constexpr sjsu::bit::Mask kMclkBitMask = sjsu::bit::CreateMaskFromRange(3);
+  sjsu::msp432p401r::P4->SEL1 =
+      sjsu::bit::Clear(sjsu::msp432p401r::P4->SEL1, kMclkBitMask);
+  sjsu::msp432p401r::P4->SEL0 =
+      sjsu::bit::Set(sjsu::msp432p401r::P4->SEL0, kMclkBitMask);
+  sjsu::msp432p401r::P4->DIR =
+      sjsu::bit::Set(sjsu::msp432p401r::P4->DIR, kMclkBitMask);
+
+  // Configure P4.4 to output HSMCLK
+  constexpr sjsu::bit::Mask kHsmclkBitMask = sjsu::bit::CreateMaskFromRange(4);
+  sjsu::msp432p401r::P4->SEL1 =
+      sjsu::bit::Clear(sjsu::msp432p401r::P4->SEL1, kHsmclkBitMask);
+  sjsu::msp432p401r::P4->SEL0 =
+      sjsu::bit::Set(sjsu::msp432p401r::P4->SEL0, kHsmclkBitMask);
+  sjsu::msp432p401r::P4->DIR =
+      sjsu::bit::Set(sjsu::msp432p401r::P4->DIR, kHsmclkBitMask);
+
+  // Configure P7.0 to output SMCLK
+  constexpr sjsu::bit::Mask kSmclkBitMask = sjsu::bit::CreateMaskFromRange(0);
+  sjsu::msp432p401r::P7->SEL1 =
+      sjsu::bit::Clear(sjsu::msp432p401r::P7->SEL1, kSmclkBitMask);
+  sjsu::msp432p401r::P7->SEL0 =
+      sjsu::bit::Set(sjsu::msp432p401r::P7->SEL0, kSmclkBitMask);
+  sjsu::msp432p401r::P7->DIR =
+      sjsu::bit::Set(sjsu::msp432p401r::P7->DIR, kSmclkBitMask);
+}
+}  // namespace
+
+int main()
+{
+  LOG_INFO("Starting MSP432P401R System Controller Demo...");
+  // Configure the on-board P1.0 LED to be initially turned on.
+  constexpr sjsu::bit::Mask kLedBitMask = sjsu::bit::CreateMaskFromRange(0);
+  sjsu::msp432p401r::P1->DIR            = sjsu::bit::Set(
+      sjsu::msp432p401r::P1->DIR, sjsu::bit::CreateMaskFromRange(0));
+  sjsu::msp432p401r::P1->OUT =
+      sjsu::bit::Set(sjsu::msp432p401r::P1->OUT, kLedBitMask);
+
+  sjsu::msp432p401r::SystemController system_controller;
+  // MCLK P4.3 should output ~12MHz
+  // HSMCLK P4.4 should output ~3MHz
+  // SMCLK P7.0 should output ~1.5MHz
+  system_controller.SetSystemClockFrequency(12_MHz);
+  system_controller.SetPeripheralClockDivider({}, 4);
+  system_controller.SetClockDivider(
+      sjsu::msp432p401r::SystemController::Clock::kLowSpeedSubsystemMaster, 8);
+
+  sjsu::cortex::SystemTimer system_timer;
+  system_timer.SetTickFrequency(config::kRtosFrequency);
+
+  ConfigureClockOutPins();
+
+  while (true)
+  {
+    sjsu::msp432p401r::P1->OUT =
+        sjsu::bit::Toggle(sjsu::msp432p401r::P1->OUT, kLedBitMask);
+    sjsu::Delay(1s);
+  }
+
+  return 0;
+}
--- library/L1_Peripheral/msp432p401r/test/system_controller_test.cpp
+++ library/L1_Peripheral/msp432p401r/test/system_controller_test.cpp
@@ -12,19 +12,113 @@ EMIT_ALL_METHODS(SystemController);
 TEST_CASE("Testing msp432p401r SystemController",
           "[msp432p401r-system-controller]")
 {
+  // Simulate local version of clock system (CS) registers.
+  CS_Type local_cs;
+  memset(&local_cs, 0, sizeof(local_cs));
+  SystemController::clock_system = &local_cs;
+  // Simulate local version of tag length value (TLV) registers.
+  TLV_Type local_tlv;
+  memset(&local_tlv, 0, sizeof(local_tlv));
+  SystemController::device_descriptors = &local_tlv;
+
+  constexpr std::array kClockDividers = { 1, 2, 4, 8, 16, 32, 64, 128 };
   SystemController system_controller;
 
-  SECTION("SetSystemClockFrequency") {}
-  SECTION("SetPeripheralClockDivider") {}
-  SECTION("GetPeripheralClockDivider") {}
-  SECTION("GetSystemFrequency")
+  SECTION("Set/Get System clock frequency")
   {
-    constexpr units::frequency::hertz_t kExpectedCpuSpeed = 3_MHz;
-    CHECK(system_controller.GetSystemFrequency() <= kExpectedCpuSpeed);
+    constexpr std::array kExpectedSpeeds = { 1_MHz,  3_MHz,  6_MHz,
+                                             14_MHz, 28_MHz, 48_MHz };
+    for (uint8_t i = 0; i < kExpectedSpeeds.size(); i++)
+    {
+      INFO("Testing SetSystemClockFrequency with frequency: "
+           << kExpectedSpeeds[i].to<uint32_t>() << " MHz");
+
+      system_controller.SetSystemClockFrequency(kExpectedSpeeds[i]);
+
+      const uint8_t kExpectedFrequencySelect = static_cast<uint8_t>(i);
+      const bool kDcoEnabled =
+          bit::Read(local_cs.CTL0, SystemController::Control0Register::kEnable);
+      const uint8_t kFrequencySelect = bit::Extract(
+          local_cs.CTL0, SystemController::Control0Register::kFrequencySelect);
+      CHECK(kDcoEnabled);
+      CHECK(kExpectedFrequencySelect == kFrequencySelect);
+      CHECK(system_controller.GetSystemFrequency() == kExpectedSpeeds[i]);
+
+      constexpr auto kExpectedMasterClockSource =
+          SystemController::Oscillator::kDigitallyControlled;
+      const auto kMasterClockSource = bit::Extract(
+          local_cs.CTL1,
+          SystemController::Control1Register::kMasterClockSourceSelect);
+      CHECK(kMasterClockSource == Value(kExpectedMasterClockSource));
+
+      constexpr auto kExpectedSubsystemMasterClockSource =
+          SystemController::Oscillator::kDigitallyControlled;
+      const auto kSubsystemMasterClockSource = bit::Extract(
+          local_cs.CTL1,
+          SystemController::Control1Register::kSubsystemClockSourceSelect);
+      CHECK(kSubsystemMasterClockSource ==
+            Value(kExpectedSubsystemMasterClockSource));
+    }
   }
-  SECTION("IsPeripheralPoweredUp") {}
-  SECTION("PowerUpPeripheral") {}
-  SECTION("PowerDownPeripheral") {}
+
+  SECTION("Set/Get Peripheral clock divider")
+  {
+    for (uint8_t i = 0; i < kClockDividers.size(); i++)
+    {
+      INFO("peripheral_divider: " << kClockDividers[i]);
+      system_controller.SetPeripheralClockDivider({}, kClockDividers[i]);
+
+      const uint8_t kExpectedDividerSelect = i;
+      const uint8_t kDividerSelect         = bit::Extract(
+          local_cs.CTL1,
+          SystemController::Control1Register::kSubsystemClockDividerSelect);
+      CHECK(kDividerSelect == kExpectedDividerSelect);
+      CHECK(system_controller.GetPeripheralClockDivider({}) ==
+            kClockDividers[i]);
+    }
+  }
+
+  SECTION("Set clock divider")
+  {
+    constexpr std::array kClocks = {
+      SystemController::Clock::kAuxiliary,
+      SystemController::Clock::kMaster,
+      SystemController::Clock::kSubsystemMaster,
+      SystemController::Clock::kLowSpeedSubsystemMaster,
+    };
+    constexpr bit::Mask kDividerSelectMasks[] = {
+      SystemController::Control1Register::kAuxiliaryClockDividerSelect,
+      SystemController::Control1Register::kMasterClockDividerSelect,
+      SystemController::Control1Register::kSubsystemClockDividerSelect,
+      SystemController::Control1Register::kLowSpeedSubsystemClockDividerSelect,
+    };
+
+    for (size_t clock_index = 0; clock_index < kClocks.size(); clock_index++)
+    {
+      const auto kClock = SystemController::Clock(clock_index);
+      for (size_t divider_index = 0; divider_index < kClockDividers.size();
+           divider_index++)
+      {
+        INFO("clock_index: " << clock_index);
+        INFO("divider_index: " << divider_index);
+        system_controller.SetClockDivider(kClock,
+                                          kClockDividers[divider_index]);
+
+        const uint8_t kExpectedDividerSelect = divider_index;
+        const uint8_t kDividerSelect =
+            bit::Extract(local_cs.CTL1, kDividerSelectMasks[clock_index]);
+        CHECK(kDividerSelect == kExpectedDividerSelect);
+      }
+    }
+  }
+
+  SECTION("Is peripheral powered up")
+  {
+    CHECK(!system_controller.IsPeripheralPoweredUp({}));
+  }
+
+  SystemController::clock_system       = msp432p401r::CS;
+  SystemController::device_descriptors = msp432p401r::TLV;
 }
 }  // namespace msp432p401r
 }  // namespace sjsu
