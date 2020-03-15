78a5b13f026750f42ad2c805ad4b187911029072
Khalil Estell <kammcecorp@gmail.com>
2020-03-14 17:15:06 -0700

    Add Accelerometer Docs
    Resolves #738
    Resolves #788

--- library/L2_HAL/sensors/movement/accelerometer.hpp
+++ library/L2_HAL/sensors/movement/accelerometer.hpp
@@ -1,19 +1,70 @@
 #pragma once
 
+#include <cmath>
 #include <cstdint>
 
+// TODO(#1080): Refactor Accelerometer
 namespace sjsu
 {
+/// @ingroup movement
+/// Abstract interface for devices that behave as accelerometers.
+/// Accelerometers are devices that can measure acceleration in X, Y, or Z axis.
+/// On the earth's surface, if the accelerometer is held still, and it is
+/// oriented flat on one of its axes, it will measure approximately 9.8 m/s^2 on
+/// that axis.
 class Accelerometer
 {
  public:
-  virtual bool Initialize()                           = 0;
-  virtual int16_t X() const                           = 0;
-  virtual int16_t Y() const                           = 0;
-  virtual int16_t Z() const                           = 0;
-  virtual float Pitch() const                         = 0;
-  virtual float Roll() const                          = 0;
-  virtual int GetFullScaleRange() const               = 0;
+  /// Initialize peripherals required to communicate with device, enables
+  /// accelerometer device. After running this function, code can read from the
+  /// X, Y, and Z methods as well as configure other settings of the device.
+  /// This method must be called before any others are called in this class.
+  virtual bool Initialize() = 0;
+
+  /// @return the acceleration on the X axis.
+  virtual int32_t X() const = 0;
+
+  /// @return the acceleration on the Y axis.
+  virtual int32_t Y() const = 0;
+
+  /// @return the acceleration on the Z axis.
+  virtual int32_t Z() const = 0;
+
+  /// @return the scale multiplier of gravity. If this returns 1, then the
+  /// maximum returned acceleration will be 9.8 m/s^2. If it is 2, then the
+  /// maximum acceleration value would be 2 time that.
+  virtual int GetFullScaleRange() const = 0;
+
+  /// Set the full scale range
+  /// @param range_value - full scale multiplier. If set to 2 then the maximum
+  /// value of any axis will represent 2 x 9.8m/s^2. If it was set to 1 then the
+  /// maximum value of any axis will represent 9.8m/s^2.
   virtual void SetFullScaleRange(uint8_t range_value) = 0;
+
+  /// Computes the pitch orientation of the accelerometer
+  ///
+  /// @return the degrees of pitch from -90 to 90 degrees as a float.
+  float Pitch() const
+  {
+    static constexpr float kRadiansToDegree = 180.0f / 3.14f;
+    float x                                 = static_cast<float>(X());
+    float y                                 = static_cast<float>(Y());
+    float z                                 = static_cast<float>(Z());
+    float pitch_numerator                   = x * -1.0f;
+    float pitch_denominator                 = sqrtf((y * y) + (z * z));
+    float pitch = atan2f(pitch_numerator, pitch_denominator) * kRadiansToDegree;
+    return pitch;
+  }
+
+  /// Computes the roll orientation of the device.
+  ///
+  /// @return the degrees of roll in -90 to 90 degrees
+  float Roll() const
+  {
+    static constexpr float kRadiansToDegree = 180.0f / 3.14f;
+    float y                                 = static_cast<float>(Y());
+    float z                                 = static_cast<float>(Z());
+    return (atan2f(y, z) * kRadiansToDegree);
+  }
 };
 }  // namespace sjsu
--- library/L2_HAL/sensors/movement/accelerometer/mma8452q.hpp
+++ library/L2_HAL/sensors/movement/accelerometer/mma8452q.hpp
@@ -1,114 +1,114 @@
 #pragma once
 
 #include <cstdint>
-#include <cmath>
 
 #include "L1_Peripheral/i2c.hpp"
 #include "L2_HAL/sensors/movement/accelerometer.hpp"
 
 namespace sjsu
 {
+/// Driver for the MMA8452Q 3-axis accelerometer
 class Mma8452q : public Accelerometer
 {
  public:
-  static constexpr uint16_t kDataOffset         = 16;
-  static constexpr float kRadiansToDegree       = 180.0f / 3.14f;
-  static constexpr uint8_t kWhoAmIExpectedValue = 0x2a;
-  static constexpr uint8_t kMsbShift            = 8;
-
-  // in units of 9.8 m/s^2 or "g"
-  static constexpr int kMaxAccelerationScale[4]         = { 2, 4, 8, -1 };
-  static constexpr uint8_t kSetMaxAccelerationScale[16] = {
-    0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
-    0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-  };
-
-  enum RegisterMap : uint8_t
+  /// Map of all of the used device addresses in this driver.
+  enum RegisterAddress : uint8_t
   {
-    kStatus     = 0x00,
-    kX          = 0x01,
-    kY          = 0x03,
-    kZ          = 0x05,
-    kWhoAmI     = 0x0d,
+    /// Device status register address
+    kStatus = 0x00,
+    /// Register address of the the first byte of the X axis
+    kX = 0x01,
+    /// Register address of the the first byte of the Y axis
+    kY = 0x03,
+    /// Register address of the the first byte of the Z axis
+    kZ = 0x05,
+    /// Device ID register address
+    kWhoAmI = 0x0d,
+    /// Device configuration starting address
     kDataConfig = 0x0e
   };
 
+  /// @param i2c - i2c peripheral used to commnicate with device.
+  /// @param address - device address.
   explicit constexpr Mma8452q(const I2c & i2c, uint8_t address = 0x1c)
-      : i2c_(i2c),
-        accelerometer_address_(address)
+      : i2c_(i2c), accelerometer_address_(address)
   {
   }
+
   bool Initialize() override
   {
     i2c_.Initialize();
     i2c_.Write(accelerometer_address_, { 0x2A, 0x01 });
     uint8_t who_am_i_received_value;
-    uint8_t identity_register = RegisterMap::kWhoAmI;
-    i2c_.WriteThenRead(accelerometer_address_, &identity_register,
-                        sizeof(identity_register), &who_am_i_received_value,
-                        sizeof(who_am_i_received_value));
+    uint8_t identity_register = RegisterAddress::kWhoAmI;
+    i2c_.WriteThenRead(accelerometer_address_,
+                       &identity_register,
+                       sizeof(identity_register),
+                       &who_am_i_received_value,
+                       sizeof(who_am_i_received_value));
+
+    constexpr uint8_t kWhoAmIExpectedValue = 0x2a;
     return (who_am_i_received_value == kWhoAmIExpectedValue);
   }
-  int16_t GetAxisValue(uint8_t register_number) const
+
+  int32_t X() const override
   {
-    int tilt_reading;
-    int16_t axis_tilt;
-    uint8_t tilt_val[2];
-    i2c_.WriteThenRead(accelerometer_address_, { register_number }, tilt_val,
-                        sizeof(tilt_val));
-    tilt_reading = (tilt_val[0] << kMsbShift) | tilt_val[1];
-    axis_tilt    = static_cast<int16_t>(tilt_reading);
-    return static_cast<int16_t>(axis_tilt / kDataOffset);
+    return GetAxisValue(RegisterAddress::kX);
   }
-  int16_t X() const override
+
+  int32_t Y() const override
   {
-    return GetAxisValue(RegisterMap::kX);
+    return GetAxisValue(RegisterAddress::kY);
   }
-  int16_t Y() const override
+
+  int32_t Z() const override
   {
-    return GetAxisValue(RegisterMap::kY);
+    return GetAxisValue(RegisterAddress::kZ);
   }
-  int16_t Z() const override
-  {
-    return GetAxisValue(RegisterMap::kZ);
-  }
-  float Pitch() const override
-  {
-    float x                 = static_cast<float>(X());
-    float y                 = static_cast<float>(Y());
-    float z                 = static_cast<float>(Z());
-    float pitch_numerator   = x * -1.0f;
-    float pitch_denominator = sqrtf((y * y) + (z * z));
-    float pitch = atan2f(pitch_numerator, pitch_denominator) * kRadiansToDegree;
-    return pitch;
-  }
-  float Roll() const override
-  {
-    float y = static_cast<float>(Y());
-    float z = static_cast<float>(Z());
-    return (atan2f(y, z) * kRadiansToDegree);
-  }
+
   int GetFullScaleRange() const override
   {
-    uint8_t config_reg = RegisterMap::kDataConfig;
+    static constexpr int kMaxAccelerationScale[4] = { 2, 4, 8, -1 };
     uint8_t full_scale_value;
-    i2c_.WriteThenRead(accelerometer_address_, { config_reg },
-                        &full_scale_value, sizeof(full_scale_value));
+    i2c_.WriteThenRead(accelerometer_address_,
+                       { RegisterAddress::kDataConfig },
+                       &full_scale_value,
+                       sizeof(full_scale_value));
     full_scale_value &= 0x03;
     int range = kMaxAccelerationScale[full_scale_value];
     return range;
   }
   void SetFullScaleRange(uint8_t range_value) override
   {
+    // in units of 9.8 m/s^2 or "g"
+    static constexpr uint8_t kSetMaxAccelerationScale[16] = {
+      0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+      0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    };
+
     range_value &= 0x0f;
-    uint8_t config_reg                 = RegisterMap::kDataConfig;
-    uint8_t send_range                 = kSetMaxAccelerationScale[range_value];
-    uint8_t full_scale_write_buffer[2] = { config_reg, send_range };
-    i2c_.Write(accelerometer_address_, full_scale_write_buffer,
-                sizeof(full_scale_write_buffer));
+    i2c_.Write(accelerometer_address_,
+               { RegisterAddress::kDataConfig,
+                 kSetMaxAccelerationScale[range_value] });
   }
 
  private:
+  int16_t GetAxisValue(uint8_t register_number) const
+  {
+    constexpr uint16_t kDataOffset = 16;
+    constexpr uint8_t kMsbShift    = 8;
+    int tilt_reading;
+    int16_t axis_tilt;
+    uint8_t tilt_val[2];
+    i2c_.WriteThenRead(accelerometer_address_,
+                       { register_number },
+                       tilt_val,
+                       sizeof(tilt_val));
+    tilt_reading = (tilt_val[0] << kMsbShift) | tilt_val[1];
+    axis_tilt    = static_cast<int16_t>(tilt_reading);
+    return static_cast<int16_t>(axis_tilt / kDataOffset);
+  }
+
   const I2c & i2c_;
   uint8_t accelerometer_address_;
 };
