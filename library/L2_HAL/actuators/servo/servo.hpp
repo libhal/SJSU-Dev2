5000f9a8855f513782bb9b5c489b1e59c928195d
Khalil Estell <kammcecorp@gmail.com>
2020-03-20 18:01:17 -0700

    Add Servo Docs
    Resolves #728

--- library/L2_HAL/actuators/servo/servo.hpp
+++ library/L2_HAL/actuators/servo/servo.hpp
@@ -8,16 +8,27 @@
 
 namespace sjsu
 {
+/// RC servo controller that can control servos or other systems that can
+/// respond to such signals.
 class Servo
 {
  public:
+  /// Typical Frequency of a generic hobby RC servo.
   static constexpr units::frequency::hertz_t kDefaultFrequency = 50_Hz;
-  static constexpr std::chrono::microseconds kDefaultPeriod    = 20'000us;
-  static constexpr units::angle::degree_t kDefaultMinAngle     = 0_deg;
-  static constexpr units::angle::degree_t kDefaultMaxAngle     = 90_deg;
-  static constexpr std::chrono::microseconds kDefaultMinPulse  = 1000us;
-  static constexpr std::chrono::microseconds kDefaultMaxPulse  = 2000us;
+  /// Typical Period of a generic hobby RC servo.
+  static constexpr std::chrono::microseconds kDefaultPeriod = 20'000us;
+  /// Typical Min Angle of a generic hobby RC servo.
+  static constexpr units::angle::degree_t kDefaultMinAngle = 0_deg;
+  /// Typical Max Angle of a generic hobby RC servo.
+  static constexpr units::angle::degree_t kDefaultMaxAngle = 90_deg;
+  /// Typical Min Pulse of a generic hobby RC servo.
+  static constexpr std::chrono::microseconds kDefaultMinPulse = 1000us;
+  /// Typical Max Pulse of a generic hobby RC servo.
+  static constexpr std::chrono::microseconds kDefaultMaxPulse = 2000us;
 
+  /// Construct the servo object
+  ///
+  /// @param pwm - pwm peripheral to use to generate the PWM signal
   explicit constexpr Servo(const sjsu::Pwm & pwm)
       : servo_pwm_(pwm),
         waveform_period_(kDefaultPeriod),
@@ -28,6 +39,10 @@ class Servo
   {
   }
 
+  /// Initialize PWM peripheral and setup device to support the required
+  /// frequency.
+  ///
+  /// @param frequency -  the frequency to operate the pulses of the servo.
   virtual void Initialize(
       units::frequency::hertz_t frequency = kDefaultFrequency)
   {
@@ -35,6 +50,9 @@ class Servo
     SetFrequency(frequency);
   }
 
+  /// Set the RC servo signal's operating frequency.
+  ///
+  /// @param frequency - the frequency to operate the pulses of the servo.
   virtual void SetFrequency(
       units::frequency::hertz_t frequency = kDefaultFrequency)
   {
@@ -43,8 +61,11 @@ class Servo
         std::chrono::microseconds((1_MHz / frequency).to<uint32_t>());
   }
 
-  // Sets the minimum and maximum pulse width lengths that the class will use
-  // to clamp its pulse width output when using SetAngle.
+  /// Sets the minimum and maximum pulse width lengths that the class will use
+  /// to clamp its pulse width output when using SetAngle.
+  ///
+  /// @param lower - the minimum microseconds pulse that the servo can handle.
+  /// @param upper - the maximum microseconds pulse that the servo can handle.
   virtual void SetPulseBounds(std::chrono::microseconds lower,
                               std::chrono::microseconds upper)
   {
@@ -52,8 +73,11 @@ class Servo
     pulse_upper_bound_ = upper;
   }
 
-  // Sets your angle bounds that maps angles to microseconds when using
-  // SetAngle.
+  /// Sets your angle bounds that maps angles to microseconds when using
+  /// SetAngle.
+  ///
+  /// @param min_angle - The minimum angle to limit the servo is bound to.
+  /// @param max_angle - The maximum angle to limit the servo is bound to.
   virtual void SetAngleBounds(units::angle::degree_t min_angle,
                               units::angle::degree_t max_angle)
   {
@@ -61,6 +85,9 @@ class Servo
     max_angle_ = max_angle;
   }
 
+  /// Set the pulse width in microsecond of the RC servo signal directly.
+  ///
+  /// @param pulse_width - how long the high side of the RC pulse should be.
   virtual void SetPulseWidthInMicroseconds(
       std::chrono::microseconds pulse_width)
   {
@@ -68,7 +95,9 @@ class Servo
                             static_cast<float>(waveform_period_.count()));
   }
 
-  // Should only be used after pulse bounds and angle bounds have been set.
+  /// Should only be used after pulse bounds and angle bounds have been set.
+  ///
+  /// @param angle - angle to position the servo to.
   virtual void SetAngle(units::angle::degree_t angle)
   {
     float pulse_width = Map(angle.to<float>(),
