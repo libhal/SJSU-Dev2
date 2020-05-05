23e95e9cec1c3cde8ac1e1c3a908b6339b6e1d26
Khalil Estell <kammcecorp@gmail.com>
2020-05-05 07:36:41 -0700

    STM32F10x: Remove SVCall and PendSV interrupt registration
    Resolves #1188

--- library/L0_Platform/stm32f10x/startup.cpp
+++ library/L0_Platform/stm32f10x/startup.cpp
@@ -76,14 +76,6 @@ extern "C"
 
   void vPortSetupTimerInterrupt(void)  // NOLINT
   {
-    interrupt_controller.Enable({
-        .interrupt_request_number = sjsu::cortex::SVCall_IRQn,
-        .interrupt_handler        = vPortSVCHandler,
-    });
-    interrupt_controller.Enable({
-        .interrupt_request_number = sjsu::cortex::PendSV_IRQn,
-        .interrupt_handler        = xPortPendSVHandler,
-    });
     // Set the SystemTick frequency to the RTOS tick frequency
     // It is critical that this happens before you set the system_clock,
     // since The system_timer keeps the time that the system_clock uses to
