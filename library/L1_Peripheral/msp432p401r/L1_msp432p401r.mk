18af0bc5b8898c2c766866abce44b4580a3559b1
David Huang <huangd95@gmail.com>
2020-06-28 16:37:51 -0700

    Fixing MSP432P401R SystemController tests to be compatible with doctest (#1280)
    - Fixed the following test cases:
      - "Initialize"
      - "Set clock divider"
    
    Resolves #1271

--- library/L1_Peripheral/msp432p401r/L1_msp432p401r.mk
+++ library/L1_Peripheral/msp432p401r/L1_msp432p401r.mk
@@ -1,6 +1,3 @@
 TESTS += $(LIBRARY_DIR)/L1_Peripheral/msp432p401r/test/gpio_test.cpp
 TESTS += $(LIBRARY_DIR)/L1_Peripheral/msp432p401r/test/pin_test.cpp
-
-# TODO(#1271): Add this back when we figure out a solution to GENERATE and
-#              subcases
-NO_TEST_NEEDED += $(LIBRARY_DIR)/L1_Peripheral/msp432p401r/system_controller.hpp
+TESTS += $(LIBRARY_DIR)/L1_Peripheral/msp432p401r/test/system_controller_test.cpp
