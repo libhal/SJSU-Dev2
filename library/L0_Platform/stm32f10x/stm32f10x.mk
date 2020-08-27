b08c4aa184f14b48d1b0ee9e7cb0efbe6265661b
Khalil Estell <kammcecorp@gmail.com>
2020-08-27 12:41:29 -0700

    Make GDB FreeRTOS thread aware for all platforms
    - Add rtos flag to all openocd config files
    - Removed ? for OPENOCD_CONFIG file in makefiles as it was not working
      as expected. To change the OPENOCD_CONFIG post_library.mk file.
    - Added 4 byte symbol into ARM exceptions class to get open ocd's
      version of FreeRTOS support to operate.

--- library/L0_Platform/stm32f10x/stm32f10x.mk
+++ library/L0_Platform/stm32f10x/stm32f10x.mk
@@ -2,7 +2,7 @@ LIBRARY_STM32F10X += $(LIBRARY_DIR)/L0_Platform/stm32f10x/startup.cpp
 LIBRARY_STM32F10X += $(LIBRARY_DIR)/L0_Platform/arm_cortex/m3/ARM_CM3/port.c
 LIBRARY_STM32F10X += $(LIBRARY_DIR)/L0_Platform/arm_cortex/exceptions.cpp
 
-OPENOCD_CONFIG ?= $(LIBRARY_DIR)/L0_Platform/stm32f10x/stm32f10x.cfg
+OPENOCD_CONFIG = $(LIBRARY_DIR)/L0_Platform/stm32f10x/stm32f10x.cfg
 
 $(eval $(call BUILD_LIBRARY,libstm32f10x,LIBRARY_STM32F10X))
 
