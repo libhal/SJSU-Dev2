52f65f4494af39d5e38dd24de04de2c96b1f4361
Khalil Estell <kammcecorp@gmail.com>
2020-10-22 07:16:26 -0700

    Fix locking in stm32f10x system controller test
    Convert the usage of std::thread in the stm32f10x system controller test
    to VerifyPolling() in order to prevent it from locking up as it has done
    in the past, resulting in longer continuous integration times and random
    failures of tests.
    
    Resolves #1269
    Resolves #1211

--- library/L1_Peripheral/test/unity_test.cpp
+++ library/L1_Peripheral/test/unity_test.cpp
@@ -18,7 +18,7 @@
 // =============================================================================
 // stm32f10x implemenation test
 // =============================================================================
-// #include "L1_Peripheral/stm32f10x/test/system_controller_test.cpp"  // NOLINT
+#include "L1_Peripheral/stm32f10x/test/system_controller_test.cpp"  // NOLINT
 #include "L1_Peripheral/stm32f10x/test/gpio_test.cpp"               // NOLINT
 #include "L1_Peripheral/stm32f10x/test/pin_test.cpp"                // NOLINT
 #include "L1_Peripheral/stm32f10x/test/uart_test.cpp"               // NOLINT
