1bba6da9cb3b550489c0e7f8fca0add7e98a9298
Khalil Estell <kammcecorp@gmail.com>
2020-12-07 12:50:48 -0800

    StaticAllocator: Updated Metrics API
    Renamed methods to names that make a bit more sense.

--- library/utility/test/allocator_test.cpp
+++ library/utility/test/allocator_test.cpp
@@ -73,9 +73,9 @@ TEST_CASE("Testing StaticAllocator")
     [[maybe_unused]] void * allocate_block2 = allocator.allocate(1, 1);
 
     // Verify
-    CHECK(kCapacity == allocator.TotalCapacity());
-    CHECK(3 == allocator.BytesAllocated());
-    CHECK((kCapacity - 3) == allocator.BytesUnallocated());
+    CHECK(kCapacity == allocator.Capacity());
+    CHECK(3 == allocator.MemoryUsed());
+    CHECK((kCapacity - 3) == allocator.MemoryAvailable());
   }
 
   SECTION("Memory Metrics with alignment")
@@ -97,9 +97,9 @@ TEST_CASE("Testing StaticAllocator")
     [[maybe_unused]] void * allocate_block2 = allocator.allocate(1, 4);
 
     // Verify
-    CHECK(kCapacity == allocator.TotalCapacity());
-    CHECK(5 == allocator.BytesAllocated());
-    CHECK((kCapacity - 5) == allocator.BytesUnallocated());
+    CHECK(kCapacity == allocator.Capacity());
+    CHECK(5 == allocator.MemoryUsed());
+    CHECK((kCapacity - 5) == allocator.MemoryAvailable());
   }
 
   SECTION("Allocate maximum capacity without exception thrown")
@@ -108,7 +108,7 @@ TEST_CASE("Testing StaticAllocator")
     StaticAllocator<32> allocator;
 
     // Exercise
-    void * ptr = allocator.allocate(allocator.TotalCapacity(), 1);
+    void * ptr = allocator.allocate(allocator.Capacity(), 1);
 
     // Verify
     CheckIfPointerIsInBounds(ptr, allocator);
@@ -123,7 +123,7 @@ TEST_CASE("Testing StaticAllocator")
     // Exercise: Allocate the entire buffer's worth, so in the next step,
     // allocating just 1 more byte will cause the allocator to throw
     // std::bad_alloc.
-    void * ptr = allocator.allocate(allocator.TotalCapacity(), 1);
+    void * ptr = allocator.allocate(allocator.Capacity(), 1);
 
     // Verify
     CHECK_THROWS_AS(
