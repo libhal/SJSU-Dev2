98740f03118ca502afb072381c757b0eee72f042
Matthew Boyd <2mjboyd@gmail.com>
2021-04-25 21:31:02 -0700

    Created a v1 Filter organization schema

--- library/peripherals/stm32f10x/can.hpp
+++ library/peripherals/stm32f10x/can.hpp
@@ -30,24 +30,19 @@ class Can final : public sjsu::Can
   {
     /// The peripheral bus clock is divided by this value
     static constexpr bit::Mask kPrescalar = bit::MaskFromRange(0, 9);
-
     /// The delay from the nominal Sync point to the sample point is (this value
     /// plus one) CAN clocks.
     static constexpr bit::Mask kTimeSegment1 = bit::MaskFromRange(16, 19);
-
     /// The delay from the sample point to the next nominal sync point isCAN
     /// clocks. The nominal CAN bit time is (this value plus the value in
     /// kTimeSegment1 plus 3) CAN clocks.
     static constexpr bit::Mask kTimeSegment2 = bit::MaskFromRange(20, 22);
-
     /// Used to compensate for positive and negative edge phase errors
     static constexpr bit::Mask kSyncJumpWidth = bit::MaskFromRange(24, 25);
-
     /// bxCAN treats its own transmitted messages as received messages
     /// and stores them (if they pass acceptance filtering) in a Receive 
     /// mailbox. (pg. 659)
     static constexpr bit::Mask kLoopBackMode = bit::MaskFromRange(30);
-
     /// The bXCAN is able to recieve valid data frames and valid remote 
     /// frames, but it sends only recessive bits on the CAN bus amd it cannot 
     /// start a transmission. (pg. 658)
@@ -70,25 +65,17 @@ class Can final : public sjsu::Can
   {
     /// Indicates to the software that the CAN hardware is now in initialization mode
     static constexpr bit::Mask kInitializationAcknowledge = bit::MaskFromRange(0);
-
     /// Indicates to the software that the CAN hardware is now in Sleep mode.
     static constexpr bit::Mask kSleepAcknowledge          = bit::MaskFromRange(1);
-
     static constexpr bit::Mask kErrorInterrupt            = bit::MaskFromRange(2);
-
     static constexpr bit::Mask kWakeupInterrupt           = bit::MaskFromRange(3);
-
     static constexpr bit::Mask kSleepAcknowledgeInterrupt = bit::MaskFromRange(4);
-
     /// Indicates if the CAN is a Transmitter
     static constexpr bit::Mask kTransmitMode              = bit::MaskFromRange(8);
-
     /// Indicates if the CAN is a receiver
     static constexpr bit::Mask kRecieveMode               = bit::MaskFromRange(9);
-
     /// Holds the last value of Rx
     static constexpr bit::Mask kLastSamplePoint           = bit::MaskFromRange(10);
-
     /// Moniters the actual value of the CAN_Rx pin.
     static constexpr bit::Mask kCANRxSignal               = bit::MaskFromRange(11);
   };
@@ -143,16 +130,95 @@ class Can final : public sjsu::Can
   {
     // Indicates how many messages are pending in the recieve FIFO
     static constexpr bit::Mask kMessagesPending         = bit::MaskFromRange(0, 1);
-
     // Set by hardware when three messages are stored in the FIFO.
     static constexpr bit::Mask kIsFIFOFull              = bit::MaskFromRange(3);
-
     // Set by hardware when a new message has been released and passed the filter while the FIFO is full.
     static constexpr bit::Mask kIsFIFOOverrun           = bit::MaskFromRange(4);
-
     // Release the output mailbox of the FIFO.
     static constexpr bit::Mask kReleaseOutputMailbox    = bit::MaskFromRange(5);
   };
+
+  ////////////////////////////////////////
+  //////// Filter Registers     //////////
+  ////////////////////////////////////////
+  
+  /// CAN Filter Master Register
+  struct FilterMaster
+  {
+    static constexpr bit::Mask kInitializationMode            = bit::MaskFromRange(0);
+    static constexpr bit::Mask kCan2StartBank                 = bit::MaskFromRange(8, 13);
+  };
+
+  /// CAN Filter Bank Mode
+  struct FilterBankMode
+  {
+    static constexpr bit::Mask kActive                        = bit::MaskFromRange(0);
+    static constexpr bit::Mask kInitialization                = bit::MaskFromRange(1);
+  };
+
+  /// CAN Filter Mode Register (CAN_FM1R)
+  struct FilterMode
+  {
+    static constexpr bit::Mask kMaskMode                      = bit::MaskFromRange(0);
+    static constexpr bit::Mask kListMode                      = bit::MaskFromRange(1);
+  };
+
+  /// CAN Filter Scale Register (CAN_FS1R)
+  struct FilterScale
+  {
+    static constexpr bit::Mask kDual16BitScale                = bit::MaskFromRange(0);
+    static constexpr bit::Mask kSingle32BitScale              = bit::MaskFromRange(1);
+  };
+
+  /// CAN Filter FIFO Assignment Register (CAN_FFA1R)
+  struct FilterFIFOAssignment
+  {
+    static constexpr bit::Mask kFIFO0                         = bit::MaskFromRange(0);
+    static constexpr bit::Mask kFIFO1                         = bit::MaskFromRange(1);
+  };
+
+    /// CAN Filter Activation Register (CAN_FFA1R)
+  struct FilterActivation
+  {
+    static constexpr bit::Mask kNotActive                      = bit::MaskFromRange(0);
+    static constexpr bit::Mask kActive                         = bit::MaskFromRange(1);
+  };
+
+  struct Filter
+  {
+    static constexpr bit::Mask k0                      = bit::MaskFromRange(0);
+    static constexpr bit::Mask k1                      = bit::MaskFromRange(1);
+    static constexpr bit::Mask k2                      = bit::MaskFromRange(2);
+    static constexpr bit::Mask k3                      = bit::MaskFromRange(3);
+    static constexpr bit::Mask k4                      = bit::MaskFromRange(4);
+    static constexpr bit::Mask k5                      = bit::MaskFromRange(5);
+    static constexpr bit::Mask k6                      = bit::MaskFromRange(6);
+    static constexpr bit::Mask k7                      = bit::MaskFromRange(7);
+    static constexpr bit::Mask k8                      = bit::MaskFromRange(8);
+    static constexpr bit::Mask k9                      = bit::MaskFromRange(9);
+    static constexpr bit::Mask k10                     = bit::MaskFromRange(10);
+    static constexpr bit::Mask k11                     = bit::MaskFromRange(11);
+    static constexpr bit::Mask k12                     = bit::MaskFromRange(12);
+    static constexpr bit::Mask k13                     = bit::MaskFromRange(13);
+    static constexpr bit::Mask k14                     = bit::MaskFromRange(14);
+    static constexpr bit::Mask k15                     = bit::MaskFromRange(15);
+    static constexpr bit::Mask k16                     = bit::MaskFromRange(16);
+    static constexpr bit::Mask k17                     = bit::MaskFromRange(17);
+    static constexpr bit::Mask k18                     = bit::MaskFromRange(18);
+    static constexpr bit::Mask k19                     = bit::MaskFromRange(19);
+    static constexpr bit::Mask k20                     = bit::MaskFromRange(20);
+    static constexpr bit::Mask k21                     = bit::MaskFromRange(21);
+    static constexpr bit::Mask k22                     = bit::MaskFromRange(22);
+    static constexpr bit::Mask k23                     = bit::MaskFromRange(23);
+    static constexpr bit::Mask k24                     = bit::MaskFromRange(24);
+    static constexpr bit::Mask k25                     = bit::MaskFromRange(25);
+    static constexpr bit::Mask k26                     = bit::MaskFromRange(26);
+    static constexpr bit::Mask k27                     = bit::MaskFromRange(27);
+  };
+
+
+
+
 
   /// CANBUS Mailbox Register
   struct MailboxIdentifier
@@ -194,20 +260,8 @@ class Can final : public sjsu::Can
   };
 
   /// Contains all of the information for to control and configure a CANBUS bus
-  /// on the LPC40xx platform.
-  struct Channel_t
-  {
-
-  };
-
-  /// Container for the LPC40xx CANBUS registers
-  struct LpcRegisters_t
-  {
-
-  };
-
-  /// List of supported CANBUS channels
-  struct Channel_t  // NOLINT
+  /// on the STM32F10x platform.
+  struct Port_t  // NOLINT
   {
      /// Reference to transmit pin object
     sjsu::Pin & td_pin;
@@ -221,15 +275,14 @@ class Can final : public sjsu::Can
     /// Pin function code for receive
     uint8_t rd_function_code;
 
-    /// Pointer to the LPC CAN peripheral in memory
+    /// Pointer to the STM32F1 CAN peripheral in memory
     CAN_TypeDef * registers;
 
     /// Peripheral's ID
     sjsu::SystemController::ResourceID id;
   };
 
-
-  /// Container for the LPC40xx CANBUS registers
+ /// Container for the STM32F10x CANBUS registers
   struct LpcRegisters_t
   {
     /// TFI register contents
@@ -242,44 +295,13 @@ class Can final : public sjsu::Can
     uint32_t data_b = 0;
   };
 
-  /// List of supported CANBUS channels
-  struct Channel  // NOLINT
-  {
-   private:
-      inline static auto port1_transmit_pin = Pin(0, 12);
-      inline static auto port1_read_pin     = Pin(0, 11);
-
-      // Need to assign. the below is incorrect
-      inline static auto port2_transmit_pin = Pin(2, 8);
-      inline static auto port2_read_pin     = Pin(2, 7);
-
-    public:
-
-    /// Predefined definition for CAN1
-    inline static const Channel_t kCan1 = {
-      .td_pin           = port1_transmit_pin,
-      .td_function_code = 3,
-      .rd_pin           = port1_read_pin,
-      .rd_function_code = 0,
-      .registers        = stm32f10x::CAN1,
-      .id               = sjsu::stm32f10x::SystemController::Peripherals::kCan1,
-    };
-
-    /// Predefined definition for CAN2
-    inline static const Channel_t kCan2 = {
-      .td_pin           = port2_transmit_pin,
-      .td_function_code = 3,
-      .rd_pin           = port2_read_pin,
-      .rd_function_code = 0,
-      .registers        = stm32f10x::CAN2,
-      .id               = sjsu::stm32f10x::SystemController::Peripherals::kCan2,
-    };
+  
 
   /// Pointer to the LPC CANBUS acceptance filter peripheral in memory
   inline static LPC_CANAF_TypeDef * can_acceptance_filter_register = LPC_CANAF;
 
   /// @param channel - Which CANBUS channel to use
-  explicit constexpr Can(const Channel_t & channel) : channel_(channel) {}
+  explicit constexpr Can(const Port_t & channel) : channel_(channel) {}
 
   void ModuleInitialize() override
   {
@@ -291,7 +313,10 @@ class Can final : public sjsu::Can
     channel_.td_pin.settings.function = channel_.td_function_code;
     channel_.rd_pin.settings.function = channel_.rd_function_code;
 
-        // Enter Initalization mode in order to write to CAN registers.
+    channel_.td_pin.Initialize();
+    channel_.rd_pin.Initialize();
+
+    // Enter Initalization mode in order to write to CAN registers.
     SetMasterControl(Mode::kInitializationRequest, true);
     SetMasterControl(Mode::kNoAutomaticRetransmission, true);
     SetMasterControl(Mode::kAutomaticBussOffManagement, true);
@@ -327,8 +352,78 @@ class Can final : public sjsu::Can
                         [[maybe_unused]] uint32_t mask,
                         [[maybe_unused]] bool is_extended = false) override
   void ConfigureAcceptanceFilter(bool enable) override
+  {
+    uint32_t 
 
-    /// Enable/Disable controller modes
+    // (Dont think needed)
+    // Configure Filters (8-9 as Mask Mode) (10 - 12 as List Mode)
+    // 8 - 12 as 1 1100
+    // CAN1->FM1R |= 0x1C << 8;              
+
+    // Activate filter initialization mode (Set bit 0) 
+    channel_.registers->FMR  |=   0x1UL;
+    SetFilterBankMode(FilterBankMode::kInitialization);
+   
+    // Deactivate filter 0 (Clear bit 0) 
+    channel_.registers->FA1R &= ~(0x1UL);
+    ActivateFilter(Filter::k0, FilterActivation::kNotActive);    
+
+    // Configure filter 0 to single 32-bit scale configuration (Set bit 0)
+    channel_.registers->FS1R |=   0x1UL;
+    SetFilterScale(Filter::k0, FilterScale::kSingle32BitScale);             
+
+    // Clear filters registers to 0
+    channel_.registers->sFilterRegister[0].FR1 = 0x0UL; 
+    channel_.registers->sFilterRegister[0].FR2 = 0x0UL;
+
+    // Set filter to mask mode (Clear bit 0)
+    channel_.registers->FM1R &= ~(0x1UL);       
+    SetFilterFilteringMode(Filter::k0, FilterMode::kMaskMode);      
+
+    // Assign filter 0 to FIFO 0 (Clear bit 0) 
+    channel_.registers->FFA1R &= ~(0x1UL);	
+    SetFilterFIFOAssignment(Filter::k0, FilterFIFOAssignment::kFIFO0);		   
+
+    // Activate filter 0 (Set bit 0) 
+    channel_.registers->FA1R  |=   0x1UL;
+    ActivateFilter(Filter::k0, FilterActivation::kActive);        
+
+    // Deactivate filter initialization mode (clear bit 0)
+    channel_.registers->FMR   &= ~(0x1UL);
+    SetFilterBankMode(FilterBankMode::kActive);	       
+  }
+
+  void SetFilterBankMode(FilterBankMode mode)
+  {
+     channel_.registers->FMR =
+        bit::Insert(channel_.registers->FMR, mode, FilterMaster::kInitializationMode);
+  }
+
+  void SetFilterFilteringMode(Filter filter, FilterMode mode)
+  {
+    channel_.registers->FM1R =
+        bit::Insert(channel_.registers->FM1R, mode, filter);
+  }
+
+    void SetFilterScale(Filter filter, FilterScale scale)
+  {
+    channel_.registers->FS1R =
+        bit::Insert(channel_.registers->FS1R, scale, filter);
+  }
+
+    void SetFilterFIFOAssignment(Filter filter, FilterFIFOAssignment fifo)
+  {
+    channel_.registers->FFA1R =
+        bit::Insert(channel_.registers->FFA1R, fifo, filter);
+  }
+
+    void ActivateFilter(Filter filter, FilterActivation state)
+  {
+    channel_.registers->FA1R =
+        bit::Insert(channel_.registers->FA1R, state, filter);
+  }
+
+  /// Enable/Disable controller modes
   ///
   /// @param mode - which mode to enable/disable
   /// @param enable_mode - true if you want to enable the mode. False otherwise.
@@ -367,24 +462,24 @@ inline Can & GetCan()
     static Can can1(kCan1);
     return can1;
   }
-  // else if constexpr (port == 2)
-  // {
-  //   static auto & port2_transmit_pin = GetPin<2, 8>();
-  //   static auto & port2_read_pin     = GetPin<2, 7>();
+  else if constexpr (port == 2)
+  {
+    static auto & port2_transmit_pin = GetPin<2, 8>();
+    static auto & port2_read_pin     = GetPin<2, 7>();
 
-  //   /// Predefined definition for CAN2
-  //   static const Can::Port_t kCan2 = {
-  //     .td_pin           = port2_transmit_pin,
-  //     .td_function_code = 1,
-  //     .rd_pin           = port2_read_pin,
-  //     .rd_function_code = 1,
-  //     .registers        = lpc40xx::LPC_CAN2,
-  //     .id               = sjsu::lpc40xx::SystemController::Peripherals::kCan2,
-  //   };
+    /// Predefined definition for CAN2
+    static const Can::Port_t kCan2 = {
+      .td_pin           = port2_transmit_pin,
+      .td_function_code = 8,
+      .rd_pin           = port2_read_pin,
+      .rd_function_code = 2,
+      .registers        = stm32f10x::LPC_CAN2,
+      .id               = sjsu::stm32f10x::SystemController::Peripherals::kCan2,
+    };
 
-  //   static Can can2(kCan2);
-  //   return can2;
-  // }
+    static Can can2(kCan2);
+    return can2;
+  }
   else
   {
     static_assert(InvalidOption<port>,
