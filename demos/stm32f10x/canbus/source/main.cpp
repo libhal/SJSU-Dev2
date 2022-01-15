edae7834ab69878291170a7543d90bb5dff83e12
Linux <2mjboyd@gmail.com>
2022-01-15 15:36:21 -0800

    Tested for 100KHz, 1Mhz, and Interupt Recieving.

--- demos/stm32f10x/canbus/source/main.cpp
+++ demos/stm32f10x/canbus/source/main.cpp
@@ -19,10 +19,31 @@ int main(void)
       "If the self-test passes, then your setup is ready to be connected");
   sjsu::LogInfo(
       "to a CAN transceiver and other can nodes. The second part of this");
-  sjsu::LogInfo(
-      "example will transmit and receive messages on CAN 1.");
+  sjsu::LogInfo("example will transmit and receive messages on CAN 1.");
   sjsu::LogInfo("Initializing CAN 1 with default bit rate of 100 kBit/s...");
   sjsu::LogInfo("Can::CanSettings_t::baud_rate = kFastdBaudRate reach 1Mhz");
+
+  // Set the CanBus handler to recieve incomming messages.
+  // STM32F10x requires recieving an incomming message to clear the interrupt for it.
+  can1.settings.handler = [](sjsu::Can & can)
+  {
+    if (can.HasData())
+    {
+      auto received_message = can.Receive();
+      sjsu::LogInfo(
+          "Recieved Message -  %d | %#04x %#04x %#04x %#04x %#04x %#04x %#04x "
+          "%#04x",
+          received_message.id,
+          received_message.payload[0],
+          received_message.payload[1],
+          received_message.payload[2],
+          received_message.payload[3],
+          received_message.payload[4],
+          received_message.payload[5],
+          received_message.payload[6],
+          received_message.payload[7]);
+    }
+  };
 
   can1.ModuleInitialize();
   unsigned int test_id = 0;
@@ -40,26 +61,12 @@ int main(void)
   unsigned int count = 0;
   while (true)
   {
-    if (can1.HasData())
-      {
-        auto received_message = can1.Receive();
-        sjsu::LogInfo("Recieved Message -  %d | %#04x %#04x %#04x %#04x %#04x %#04x %#04x %#04x",
-                      received_message.id,
-                      received_message.payload[0],
-                      received_message.payload[1],
-                      received_message.payload[2],
-                      received_message.payload[3],
-                      received_message.payload[4],
-                      received_message.payload[5],
-                      received_message.payload[6],
-                      received_message.payload[7]);
-      }
     // Send new message every 3 seconds.
-    if(count == 30)
+    if (count == 30)
     {
-      // Sending: 
-        // Incrementing ID to verify validity of message.
-        // Array of 1's to check frequency with logic analyzer.
+      // Sending:
+      // Incrementing ID to verify validity of message.
+      // Array of 1's to check frequency with logic analyzer.
       sjsu::LogInfo("Sending Message - ID: %d", test_id);
       sjsu::Can::Message_t test_message;
       test_message.id         = test_id;
@@ -76,7 +83,7 @@ int main(void)
       count = 0;
       test_id++;
     }
-    
+
     count++;
     sjsu::Delay(100ms);
   }
